c6fb4167174771dd22eb98271297083fd6c87d4f
Longyi Qi <lqi+git@longyiqi.com>
2012-11-14 21:51:52 -0600

    Added double negative rule

---
+++ oclint-rules/rules/basic/DoubleNegativeRule.cpp
@@ -0,0 +1,44 @@
+#include <clang/AST/AST.h>
+
+#include "oclint/Rule.h"
+#include "oclint/RuleSet.h"
+
+class DoubleNegativeRule : public Rule<DoubleNegativeRule>
+{
+private:
+    static RuleSet rules;
+
+public:
+    virtual const string name() const
+    {
+        return "double negative";
+    }
+
+    virtual const int priority() const
+    {
+        return 2;
+    }
+
+    bool VisitUnaryOperator(UnaryOperator *unaryOperator)
+    {
+        Expr *subExpr = unaryOperator->getSubExpr();
+        while (isa<ParenExpr>(subExpr))
+        {
+            ParenExpr *parenExpr = dyn_cast<ParenExpr>(subExpr);
+            subExpr = parenExpr->getSubExpr();
+        }
+        if (isa<UnaryOperator>(subExpr))
+        {
+            UnaryOperator *subUnaryOperator = dyn_cast<UnaryOperator>(subExpr);
+            if ((unaryOperator->getOpcode() == UO_LNot || subUnaryOperator->getOpcode() == UO_LNot) ||
+                (unaryOperator->getOpcode() == UO_Not || subUnaryOperator->getOpcode() == UO_Not))
+            {
+                addViolation(unaryOperator, this);
+            }
+        }
+
+        return true;
+    }
+};
+
+RuleSet DoubleNegativeRule::rules(new DoubleNegativeRule());
