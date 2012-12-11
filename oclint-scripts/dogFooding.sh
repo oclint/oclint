81eeab56b8c3421aa754292f5c5be85790b1c0a2
Longyi Qi <lqi@pros.com>
2012-12-11 12:48:47 -0600

    Added dog fooding scripts for metrics, this project is now crying for a better build scripts

--- oclint-scripts/dogFooding.sh
+++ oclint-scripts/dogFooding.sh
@@ -7,28 +7,40 @@ LLVM_SRC="$PROJECT_ROOT/llvm"
 LLVM_BUILD="$PROJECT_ROOT/build/llvm-install"
 OCLINT_CORE_SRC="$PROJECT_ROOT/oclint-core"
 OCLINT_CORE_BUILD="$PROJECT_ROOT/build/oclint-core"
+OCLINT_METRICS_SRC="$PROJECT_ROOT/oclint-metrics"
+OCLINT_METRICS_BUILD="$PROJECT_ROOT/build/oclint-metrics"
 OCLINT_RULES_SRC="$PROJECT_ROOT/oclint-rules"
 OCLINT_RULES_BUILD="$PROJECT_ROOT/build/oclint-rules"
 OCLINT_RELEASE_BUILD="$PROJECT_ROOT/build/oclint-release"
+
 OCLINT_CORE_TMP="$PROJECT_ROOT/build/oclint-core-tmp"
 OCLINT_RULES_TMP="$PROJECT_ROOT/build/oclint-rules-tmp"
+OCLINT_METRICS_TMP="$PROJECT_ROOT/build/oclint-metrics-tmp"
 
 # create temporary directory
 mkdir -p $OCLINT_CORE_TMP
+mkdir -p $OCLINT_METRICS_TMP
 mkdir -p $OCLINT_RULES_TMP
 
 # generate compile_commands.json file
 cd $OCLINT_CORE_TMP
 cmake -D CMAKE_EXPORT_COMPILE_COMMANDS=ON -D CMAKE_CXX_COMPILER=$LLVM_BUILD/bin/clang++ -D CMAKE_C_COMPILER=$LLVM_BUILD/bin/clang -D LLVM_ROOT=$LLVM_BUILD $OCLINT_CORE_SRC
+cd $OCLINT_METRICS_TMP
+cmake -D CMAKE_EXPORT_COMPILE_COMMANDS=ON -D CMAKE_CXX_COMPILER=$LLVM_BUILD/bin/clang++ -D CMAKE_C_COMPILER=$LLVM_BUILD/bin/clang -D LLVM_ROOT=$LLVM_BUILD $OCLINT_METRICS_SRC
 cd $OCLINT_RULES_TMP
-cmake -D CMAKE_EXPORT_COMPILE_COMMANDS=ON -D CMAKE_CXX_COMPILER=$LLVM_BUILD/bin/clang++ -D CMAKE_C_COMPILER=$LLVM_BUILD/bin/clang -D LLVM_ROOT=$LLVM_BUILD -D OCLINT_SOURCE_DIR=$OCLINT_CORE_SRC -D OCLINT_BUILD_DIR=$OCLINT_CORE_BUILD $OCLINT_RULES_SRC
+cmake -D CMAKE_EXPORT_COMPILE_COMMANDS=ON -D CMAKE_CXX_COMPILER=$LLVM_BUILD/bin/clang++ -D CMAKE_C_COMPILER=$LLVM_BUILD/bin/clang -D LLVM_ROOT=$LLVM_BUILD -D OCLINT_SOURCE_DIR=$OCLINT_CORE_SRC -D OCLINT_BUILD_DIR=$OCLINT_CORE_BUILD -D OCLINT_METRICS_SOURCE_DIR=$OCLINT_METRICS_SRC -D OCLINT_METRICS_BUILD_DIR=$OCLINT_METRICS_BUILD $OCLINT_RULES_SRC
 
 # copy compile_commands.json to source folders respectively
 cp $OCLINT_CORE_TMP/compile_commands.json $OCLINT_CORE_SRC/compile_commands.json
+cp $OCLINT_METRICS_TMP/compile_commands.json $OCLINT_METRICS_SRC/compile_commands.json
 cp $OCLINT_RULES_TMP/compile_commands.json $OCLINT_RULES_SRC/compile_commands.json
 
 # dog fooding for core
 cd $OCLINT_CORE_SRC
+$OCLINT_RELEASE_BUILD/bin/oclint-json-compilation-database
+
+# dog fooding for metrics
+cd $OCLINT_METRICS_SRC
 $OCLINT_RELEASE_BUILD/bin/oclint-json-compilation-database
 
 # dog fooding for rules
@@ -37,6 +49,7 @@ $OCLINT_RELEASE_BUILD/bin/oclint-json-compilation-database
 
 # delete temporary folders
 rm -rf $OCLINT_CORE_TMP
+rm -rf $OCLINT_METRICS_TMP
 rm -rf $OCLINT_RULES_TMP
 
 # back to the current folder
