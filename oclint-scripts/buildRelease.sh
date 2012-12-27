dc037b155d4f2f79350a7370a4977166af30dc3e
Longyi Qi <lqi+git@longyiqi.com>
2012-12-27 17:37:56 -0600

    Add proper open source licenses for release build

--- oclint-scripts/buildRelease.sh
+++ oclint-scripts/buildRelease.sh
@@ -7,6 +7,8 @@ LLVM_BUILD="$PROJECT_ROOT/build/llvm-install"
 OCLINT_CORE_BUILD="$PROJECT_ROOT/build/oclint-core"
 OCLINT_RULES_BUILD="$PROJECT_ROOT/build/oclint-rules"
 OCLINT_RELEASE_BUILD="$PROJECT_ROOT/build/oclint-release"
+LLVM_SRC="$PROJECT_ROOT/llvm"
+OCLINT_CORE_SRC="$PROJECT_ROOT/oclint-core"
 OCLINT_JSON_CD_FOLDER="$PROJECT_ROOT/oclint-json-compilation-database"
 OCLINT_XCODBUILD_FOLDER="$PROJECT_ROOT/oclint-xcodebuild"
 
@@ -42,6 +44,11 @@ fi
 if [ -d "$OCLINT_XCODBUILD_FOLDER" ] && [ `uname` == "Darwin" ]; then
     cp $OCLINT_XCODBUILD_FOLDER/oclint* $OCLINT_RELEASE_BUILD/bin
 fi
+
+# licenses
+cat $OCLINT_CORE_SRC/LICENSE > $OCLINT_RELEASE_BUILD/LICENSE
+echo "" >> $OCLINT_RELEASE_BUILD/LICENSE
+head -n 44 $LLVM_SRC/LICENSE.TXT >> $OCLINT_RELEASE_BUILD/LICENSE
 
 # back to the current folder
 cd $CWD
