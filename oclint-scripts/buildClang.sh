a56224294c5fdd1f58d8c2d21ceac437c2cd41df
Longyi Qi <lqi+git@longyiqi.com>
2012-12-28 22:58:18 -0600

    Release build for 0.6

--- oclint-scripts/buildClang.sh
+++ oclint-scripts/buildClang.sh
@@ -12,7 +12,7 @@ mkdir -p $LLVM_BUILD
 cd $LLVM_BUILD
 
 # configure and build
-cmake -D CMAKE_INSTALL_PREFIX=$LLVM_INSTALL $LLVM_SRC
+cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=$LLVM_INSTALL $LLVM_SRC
 make
 make install
 
