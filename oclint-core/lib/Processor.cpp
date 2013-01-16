4bdc9498dc29646a7101a774e3fa2c05f18eadcc
Longyi Qi <lqi+git@longyiqi.com>
2013-01-16 11:06:06 -0600

    Move apply() function from RuleSet to Processor
    RuleSet becomes a pure collection of rules. It no longer is a messenger to 
    pass ASTContext and ViolationSet to all the rules it consists of.

--- oclint-core/lib/Processor.cpp
+++ oclint-core/lib/Processor.cpp
@@ -1,12 +1,16 @@
 #include "oclint/Processor.h"
 #include "oclint/Results.h"
+#include "oclint/RuleBase.h"
 #include "oclint/RuleSet.h"
 #include "oclint/ViolationSet.h"
 
 void Processor::HandleTranslationUnit(ASTContext &astContext)
 {
     ViolationSet *violationSet = new ViolationSet();
-    RuleSet::apply(astContext, violationSet);
+    for (int index = 0, numRules = RuleSet::numberOfRules(); index < numRules; index++)
+    {
+        RuleSet::getRuleAtIndex(index)->apply(astContext, violationSet);
+    }
     Results *results = Results::getInstance();
     results->add(violationSet);
 }
