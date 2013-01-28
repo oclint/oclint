531518d4756fc4936a03ced781cb22ecbd90190e
Longyi Qi <lqi+git@longyiqi.com>
2013-01-28 14:47:15 -0600

    Add tests for TextReporter and HTMLReporter

--- oclint-reporters/reporters/TextReporter.cpp
+++ oclint-reporters/reporters/TextReporter.cpp
@@ -12,24 +12,46 @@ public:
 
     virtual void report(Results *results, ostream &out)
     {
-        out << "OCLint Report" << endl << endl;
-        out << "Summary: TotalFiles=" << results->numberOfFiles() << " ";
-        out << "FilesWithViolations=" << results->numberOfFilesWithViolations() << " ";
-        out << "P1=" << results->numberOfViolationsWithPriority(1) << " ";
-        out << "P2=" << results->numberOfViolationsWithPriority(2) << " ";
-        out << "P3=" << results->numberOfViolationsWithPriority(3) << " ";
+        writeHeader(out);
+        out << endl << endl;
+        writeSummary(out, *results);
         out << endl << endl;
         vector<Violation> violationSet = results->allViolations();
         for (int index = 0, numberOfViolations = violationSet.size();
             index < numberOfViolations; index++)
         {
-            Violation violation = violationSet.at(index);
-            out << violation.path << ":" << violation.startLine << ":" << violation.startColumn;
-            const RuleBase *rule = violation.rule;
-            out << ": " << rule->name()
-                << " P" << rule->priority() << " " << violation.message << endl;
+            writeViolation(out, violationSet.at(index));
+            out << endl;
         }
-        out << endl << "[OCLint (http://oclint.org) v0.7]" << endl;
+        out << endl;
+        writeFooter(out, "0.7");
+        out << endl;
+    }
+
+    void writeHeader(ostream &out)
+    {
+        out << "OCLint Report";
+    }
+
+    void writeFooter(ostream &out, string version)
+    {
+        out << "[OCLint (http://oclint.org) v" << version << "]";
+    }
+
+    void writeSummary(ostream &out, Results &results)
+    {
+        out << "Summary: TotalFiles=" << results.numberOfFiles() << " ";
+        out << "FilesWithViolations=" << results.numberOfFilesWithViolations() << " ";
+        out << "P1=" << results.numberOfViolationsWithPriority(1) << " ";
+        out << "P2=" << results.numberOfViolationsWithPriority(2) << " ";
+        out << "P3=" << results.numberOfViolationsWithPriority(3) << " ";
+    }
+
+    void writeViolation(ostream &out, Violation &violation)
+    {
+        out << violation.path << ":" << violation.startLine << ":" << violation.startColumn;
+        const RuleBase *rule = violation.rule;
+        out << ": " << rule->name() << " P" << rule->priority() << " " << violation.message;
     }
 };
 
