2f65ddfb6926b9417b6e039892cf0dbb487e27ec
Longyi Qi <lqi+git@longyiqi.com>
2013-01-28 14:47:15 -0600

    Add tests for TextReporter and HTMLReporter

--- oclint-reporters/reporters/HTMLReporter.cpp
+++ oclint-reporters/reporters/HTMLReporter.cpp
@@ -14,6 +14,58 @@ public:
     {
         out << "<!DOCTYPE html>";
         out << "<html>";
+        writeHead(out);
+        out << "<body>";
+        out << "<h1>OCLint Report</h1>";
+        out << "<hr />";
+        out << "<h2>Summary</h2>";
+        writeSummaryTable(out, *results);
+        out << "<hr />";
+        out << "<table><thead><tr><th>File</th><th>Location</th><th>Rule Name</th>"
+            << "<th>Priority</th><th>Message</th></tr></thead><tbody>";
+        vector<Violation> violationSet = results->allViolations();
+        for (int index = 0, numberOfViolations = violationSet.size();
+            index < numberOfViolations; index++)
+        {
+            writeViolation(out, violationSet.at(index));
+        }
+        out << "</tbody></table>";
+        out << "<hr />";
+        writeFooter(out, "0.7");
+        out << "</body>";
+        out << "</html>";
+        out << endl;
+    }
+
+    void writeFooter(ostream &out, string version)
+    {
+        time_t now = time(0);
+        out << "<p>" << ctime(&now)
+            << "| Generated with <a href='http://oclint.org'>OCLint v" << version << "</a>.</p>";
+    }
+
+    void writeViolation(ostream &out, Violation &violation)
+    {
+        out << "<tr><td>" << violation.path << "</td><td>" << violation.startLine
+            << ":" << violation.startColumn << "</td>";
+        const RuleBase *rule = violation.rule;
+        out << "<td>" << rule->name() << "</td><td class='priority" << rule->priority() << "'>"
+            << rule->priority() << "</td><td>" << violation.message << "</td></tr>";
+    }
+
+    void writeSummaryTable(ostream &out, Results results)
+    {
+        out << "<table><thead><tr><th>Total Files</th><th>Files with Violations</th>"
+            << "<th>Priority 1</th><th>Priority 2</th><th>Priority 3</th></tr></thead>";
+        out << "<tbody><tr><td>" << results.numberOfFiles() << "</td><td>"
+            << results.numberOfFilesWithViolations() << "</td><td class='priority1'>"
+            << results.numberOfViolationsWithPriority(1) << "</td><td class='priority2'>"
+            << results.numberOfViolationsWithPriority(2) << "</td><td class='priority3'>"
+            << results.numberOfViolationsWithPriority(3) << "</td></tr></tbody></table>";
+    }
+
+    void writeHead(ostream &out)
+    {
         out << "<head>";
         out << "<title>OCLint Report</title>";
         out << "<style type='text/css'>"
@@ -44,38 +96,6 @@ th {                                        \
 }"
             << "</style>";
         out << "</head>";
-        out << "<body>";
-        out << "<h1>OCLint Report</h1>";
-        out << "<hr />";
-        out << "<h2>Summary</h2>";
-        out << "<table><thead><tr><th>Total Files</th><th>Files with Violations</th>"
-            << "<th>Priority 1</th><th>Priority 2</th><th>Priority 3</th></tr></thead>";
-        out << "<tbody><tr><td>" << results->numberOfFiles() << "</td><td>"
-            << results->numberOfFilesWithViolations() << "</td><td class='priority1'>"
-            << results->numberOfViolationsWithPriority(1) << "</td><td class='priority2'>"
-            << results->numberOfViolationsWithPriority(2) << "</td><td class='priority3'>"
-            << results->numberOfViolationsWithPriority(3) << "</td></tr></tbody></table>";
-        out << "<hr />";
-        out << "<table><thead><tr><th>File</th><th>Location</th><th>Rule Name</th>"
-            << "<th>Priority</th><th>Message</th></tr></thead><tbody>";
-        vector<Violation> violationSet = results->allViolations();
-        for (int index = 0, numberOfViolations = violationSet.size();
-            index < numberOfViolations; index++)
-        {
-            Violation violation = violationSet.at(index);
-            out << "<tr><td>" << violation.path << "</td><td>" << violation.startLine
-                << ":" << violation.startColumn << "</td>";
-            const RuleBase *rule = violation.rule;
-            out << "<td>" << rule->name() << "</td><td class='priority" << rule->priority() << "'>"
-                << rule->priority() << "</td><td>" << violation.message << "</td></tr>";
-        }
-        out << "</tbody></table>";
-        out << "<hr />";
-        time_t now = time(0);
-        out << ctime(&now)
-            << "| Generated with <a href='http://oclint.org'>OCLint v0.7</a>.</p>";
-        out << "</body>";
-        out << "</html>" << endl;
     }
 };
 
---
+++ oclint-reporters/test/TextReporterTest.cpp
@@ -0,0 +1,77 @@
+#include <sstream>
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "TextReporter.cpp"
+
+using namespace std;
+using namespace ::testing;
+
+class MockRuleBase : public RuleBase
+{
+public:
+    MOCK_METHOD0(apply, void());
+    MOCK_CONST_METHOD0(name, const string());
+    MOCK_CONST_METHOD0(priority, const int());
+};
+
+class TextReporterTest : public ::testing::Test
+{
+protected:
+    virtual void setUp()
+    {
+        TextReporter reporter;
+    }
+
+    TextReporter reporter;
+};
+
+TEST_F(TextReporterTest, PropertyTest)
+{
+    EXPECT_THAT(reporter.name(), StrEq("text"));
+}
+
+TEST_F(TextReporterTest, WriteHeader)
+{
+    ostringstream oss;
+    reporter.writeHeader(oss);
+    EXPECT_THAT(oss.str(), StrEq("OCLint Report"));
+}
+
+TEST_F(TextReporterTest, WriteFooter)
+{
+    ostringstream oss;
+    reporter.writeFooter(oss, "-test");
+    EXPECT_THAT(oss.str(), StrEq("[OCLint (http://oclint.org) v-test]"));
+}
+
+TEST_F(TextReporterTest, WriteSummary)
+{
+    Results *restults = Results::getInstance();
+    ostringstream oss;
+    reporter.writeSummary(oss, *restults);
+    EXPECT_THAT(oss.str(), StartsWith("Summary:"));
+    EXPECT_THAT(oss.str(), HasSubstr("TotalFiles=0"));
+    EXPECT_THAT(oss.str(), HasSubstr("FilesWithViolations=0"));
+    EXPECT_THAT(oss.str(), HasSubstr("P1=0"));
+    EXPECT_THAT(oss.str(), HasSubstr("P2=0"));
+    EXPECT_THAT(oss.str(), HasSubstr("P3=0"));
+}
+
+TEST_F(TextReporterTest, WriteViolation)
+{
+    RuleBase *rule = new MockRuleBase();
+    Violation violation(rule, "test path", 1, 2, 3, 4, "test message");
+    ostringstream oss;
+    reporter.writeViolation(oss, violation);
+    EXPECT_THAT(oss.str(), HasSubstr("test path"));
+    EXPECT_THAT(oss.str(), HasSubstr("1:2"));
+    EXPECT_THAT(oss.str(), HasSubstr("test message"));
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
