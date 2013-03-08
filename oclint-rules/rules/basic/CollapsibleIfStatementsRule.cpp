12ffc78fff34e7e96c3bb31b5b7b4f5ac1de6122
Longyi Qi <lqi+git@longyiqi.com>
2013-03-08 04:50:23 -0600

    Fixed the false positive mentioned in #10

--- oclint-rules/rules/basic/CollapsibleIfStatementsRule.cpp
+++ oclint-rules/rules/basic/CollapsibleIfStatementsRule.cpp
@@ -11,6 +11,28 @@ private:
         return compoundStmt->size() == 1 && isa<IfStmt>(*(compoundStmt->body_begin()));
     }
 
+    IfStmt *getInnerIfStmt(IfStmt *ifStmt)
+    {
+        if (isa<IfStmt>(ifStmt->getThen()))
+        {
+            return dyn_cast<IfStmt>(ifStmt->getThen());
+        }
+        if (isa<CompoundStmt>(ifStmt->getThen()))
+        {
+            CompoundStmt *compoundStmt = dyn_cast<CompoundStmt>(ifStmt->getThen());
+            if (compoundStmtContainsOnlyOneIfStmt(compoundStmt))
+            {
+                return dyn_cast<IfStmt>(*(compoundStmt->body_begin()));
+            }
+        }
+        return NULL;
+    }
+
+    bool checkElseBranch(IfStmt *outerIf, IfStmt *innerIf)
+    {
+        return outerIf->getElse() && innerIf->getElse();
+    }
+
 public:
     virtual const string name() const
     {
@@ -24,9 +46,8 @@ public:
 
     bool VisitIfStmt(IfStmt *ifStmt)
     {
-        if (isa<IfStmt>(ifStmt->getThen()) ||
-            (isa<CompoundStmt>(ifStmt->getThen()) &&
-                compoundStmtContainsOnlyOneIfStmt(dyn_cast<CompoundStmt>(ifStmt->getThen()))))
+        IfStmt *innerIf = getInnerIfStmt(ifStmt);
+        if (innerIf && !checkElseBranch(ifStmt, innerIf))
         {
             addViolation(ifStmt, this);
         }
--- oclint-rules/test/basic/CollapsibleIfStatementsRuleTest.cpp
+++ oclint-rules/test/basic/CollapsibleIfStatementsRuleTest.cpp
@@ -31,6 +31,50 @@ TEST(CollapsibleIfStatementsRuleTest, IfThenIsCompoundContainsOnlyIf)
         0, 1, 12, 1, 31);
 }
 
+/*
+ Tests for the false positive found by Stephan Esch
+ Details at https://github.com/oclint/oclint/issues/10
+ When we find collapsible if statements, we need to check the existence of their else branch
+ OuterElse InnerElse Emit
+     1         1       0
+     1         0       1
+     0         1       1
+     0         0       1
+*/
+
+TEST(CollapsibleIfStatementsRuleTest, BothOuterAndInnerIfHasElseExistence)
+{
+    testRuleOnCode(new CollapsibleIfStatementsRule(), "void m() { if (1) { if (0) {} else {} } else {} }");
+}
+
+TEST(CollapsibleIfStatementsRuleTest, OuterIfHasElseInnerDoesNot)
+{
+    testRuleOnCode(new CollapsibleIfStatementsRule(), "void m() { if (1) { if (0) {} } else {} }",
+        0, 1, 12, 1, 39);
+}
+
+TEST(CollapsibleIfStatementsRuleTest, OuterIfHasNoElseInnerDoes)
+{
+    testRuleOnCode(new CollapsibleIfStatementsRule(), "void m() { if (1) { if (0) {} else {} } }",
+        0, 1, 12, 1, 39);
+}
+
+TEST(CollapsibleIfStatementsRuleTest, OuterIfHasElseIf)
+{
+    testRuleOnCode(new CollapsibleIfStatementsRule(), "void m() { if (1) { if (0) {} else {} } else if(1) {} }");
+}
+
+TEST(CollapsibleIfStatementsRuleTest, ElseIfCanBeCollapsed)
+{
+    testRuleOnCode(new CollapsibleIfStatementsRule(), "void m() { if (1) {} else if(1) { if (0) {} } }",
+        0, 1, 27, 1, 45);
+}
+
+TEST(CollapsibleIfStatementsRuleTest, ElseIfHasElseAndNestedIfHasElse)
+{
+    testRuleOnCode(new CollapsibleIfStatementsRule(), "void m() { if (1) {} else if (1) { if (0) {} else {} } else {} }");
+}
+
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleMock(&argc, argv);
