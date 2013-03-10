4066f57040389386f16a21e8fff0cb3c42d767b6
Longyi Qi <lqi+git@longyiqi.com>
2013-03-10 08:17:15 -0500

    Convert tab to space

--- oclint-reporters/reporters/PMDReporter.cpp
+++ oclint-reporters/reporters/PMDReporter.cpp
@@ -6,50 +6,50 @@
 class PMDReporter : public Reporter
 {
 public:
-	virtual const string name() const
-	{
-		return "pmd";
-	}
-	
-	virtual void report(Results *results, ostream &out)
-	{
-		writeHeader(out, Version::identifier());
+    virtual const string name() const
+    {
+        return "pmd";
+    }
 
-		vector<Violation> violationSet = results->allViolations();
-		for (int index = 0, numberOfViolations = violationSet.size(); index < numberOfViolations; index++)
-		{
-			writeViolation(out, violationSet.at(index));
-			out << endl;
-		}
+    virtual void report(Results *results, ostream &out)
+    {
+        writeHeader(out, Version::identifier());
 
-		writeFooter(out);
-	}
-	
-	void writeHeader(ostream &out, string version)
-	{
-		out << "<pmd version=\"" << version << "\">";
-	}
-	
-	void writeFooter(ostream &out)
-	{
-		out << "</pmd>";
-	}
-	
-	void writeViolation(ostream &out, Violation &violation)
-	{
-		out << "<file name=\"" << violation.path << "\">" << endl;
-		out << "<violation ";
-		out << "begincolumn=\"" << violation.startColumn << "\" ";
-		out << "beginline=\"" << violation.startLine << "\" ";
-		const RuleBase *rule = violation.rule;
-		out << "priority=\"" << 2 * rule->priority() - 1 << "\" rule=\"" << rule->name() << "\">" << endl;
-		out << violation.message << endl;
-		out << "</violation>" << endl;
-		out << "</file>" << endl;
-	}
+        vector<Violation> violationSet = results->allViolations();
+        for (int index = 0, numberOfViolations = violationSet.size(); index < numberOfViolations; index++)
+        {
+            writeViolation(out, violationSet.at(index));
+            out << endl;
+        }
+
+        writeFooter(out);
+    }
+
+    void writeHeader(ostream &out, string version)
+    {
+        out << "<pmd version=\"" << version << "\">";
+    }
+
+    void writeFooter(ostream &out)
+    {
+        out << "</pmd>";
+    }
+
+    void writeViolation(ostream &out, Violation &violation)
+    {
+        out << "<file name=\"" << violation.path << "\">" << endl;
+        out << "<violation ";
+        out << "begincolumn=\"" << violation.startColumn << "\" ";
+        out << "beginline=\"" << violation.startLine << "\" ";
+        const RuleBase *rule = violation.rule;
+        out << "priority=\"" << 2 * rule->priority() - 1 << "\" rule=\"" << rule->name() << "\">" << endl;
+        out << violation.message << endl;
+        out << "</violation>" << endl;
+        out << "</file>" << endl;
+    }
 };
 
 extern "C" Reporter* create()
 {
-	return new PMDReporter();
+    return new PMDReporter();
 }
--- oclint-reporters/test/PMDReporterTest.cpp
+++ oclint-reporters/test/PMDReporterTest.cpp
@@ -11,59 +11,59 @@ using namespace ::testing;
 class MockRuleBase : public RuleBase
 {
 public:
-	MOCK_METHOD0(apply, void());
-	MOCK_CONST_METHOD0(name, const string());
-	MOCK_CONST_METHOD0(priority, const int());
+    MOCK_METHOD0(apply, void());
+    MOCK_CONST_METHOD0(name, const string());
+    MOCK_CONST_METHOD0(priority, const int());
 };
 
 class TextReporterTest : public ::testing::Test
 {
 protected:
-	virtual void setUp()
-	{
-		PMDReporter reporter;
-	}
+    virtual void setUp()
+    {
+        PMDReporter reporter;
+    }
 
-	PMDReporter reporter;
+    PMDReporter reporter;
 };
 
 TEST_F(TextReporterTest, PropertyTest)
 {
-	EXPECT_THAT(reporter.name(), StrEq("pmd"));
+    EXPECT_THAT(reporter.name(), StrEq("pmd"));
 }
 
 TEST_F(TextReporterTest, WriteHeader)
 {
-	ostringstream oss;
-	reporter.writeHeader(oss, "test");
-	//     EXPECT_THAT(oss.str(), StrEq("OCLint Report"));
-	EXPECT_THAT(oss.str(), StrEq("<pmd version=\"test\">"));
+    ostringstream oss;
+    reporter.writeHeader(oss, "test");
+    //     EXPECT_THAT(oss.str(), StrEq("OCLint Report"));
+    EXPECT_THAT(oss.str(), StrEq("<pmd version=\"test\">"));
 }
 
 TEST_F(TextReporterTest, WriteFooter)
 {
-	ostringstream oss;
-	reporter.writeFooter(oss);
-	EXPECT_THAT(oss.str(), StrEq("</pmd>"));
+    ostringstream oss;
+    reporter.writeFooter(oss);
+    EXPECT_THAT(oss.str(), StrEq("</pmd>"));
 }
 
 TEST_F(TextReporterTest, WriteViolation)
 {
-	RuleBase *rule = new MockRuleBase();
-	Violation violation(rule, "test path", 1, 2, 3, 4, "test message");
-	ostringstream oss;
-	reporter.writeViolation(oss, violation);
-	EXPECT_THAT(oss.str(), HasSubstr("test path"));
-	EXPECT_THAT(oss.str(), HasSubstr("<file name=\"test path\">"));
-	EXPECT_THAT(oss.str(), HasSubstr("<violation"));
-	EXPECT_THAT(oss.str(), HasSubstr("begincolumn="));
-	EXPECT_THAT(oss.str(), HasSubstr("beginline="));
-	EXPECT_THAT(oss.str(), HasSubstr("rule"));
-	EXPECT_THAT(oss.str(), HasSubstr("test message"));
+    RuleBase *rule = new MockRuleBase();
+    Violation violation(rule, "test path", 1, 2, 3, 4, "test message");
+    ostringstream oss;
+    reporter.writeViolation(oss, violation);
+    EXPECT_THAT(oss.str(), HasSubstr("test path"));
+    EXPECT_THAT(oss.str(), HasSubstr("<file name=\"test path\">"));
+    EXPECT_THAT(oss.str(), HasSubstr("<violation"));
+    EXPECT_THAT(oss.str(), HasSubstr("begincolumn="));
+    EXPECT_THAT(oss.str(), HasSubstr("beginline="));
+    EXPECT_THAT(oss.str(), HasSubstr("rule"));
+    EXPECT_THAT(oss.str(), HasSubstr("test message"));
 }
 
 int main(int argc, char **argv)
 {
-	::testing::InitGoogleMock(&argc, argv);
-	return RUN_ALL_TESTS();
+    ::testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
 }
