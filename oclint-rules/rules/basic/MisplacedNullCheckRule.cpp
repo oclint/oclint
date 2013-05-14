3f955659cc3024225b95195f2e01fe1d6921513a
Longyi Qi <lqi+git@longyiqi.com>
2013-05-14 18:02:28 -0500

    Extract same logic into AbstractNullCheckRule for BrokenNullCheckRule and MisplacedNullCheckRule

--- oclint-rules/rules/basic/MisplacedNullCheckRule.cpp
+++ oclint-rules/rules/basic/MisplacedNullCheckRule.cpp
@@ -1,76 +1,12 @@
 #include "oclint/AbstractASTVisitorRule.h"
 #include "oclint/RuleSet.h"
 
-class MisplacedNullCheckRule : public AbstractASTVisitorRule<MisplacedNullCheckRule>
+#include "AbstractNullCheckRule.h"
+
+class MisplacedNullCheckRule : public AbstractNullCheckRule<MisplacedNullCheckRule>
 {
 private:
     static RuleSet rules;
-
-    bool isNullToPointer(Expr *expr)
-    {
-        ImplicitCastExpr *implicitCastExpr = dyn_cast<ImplicitCastExpr>(expr);
-        if (implicitCastExpr)
-        {
-            CastKind castKind = implicitCastExpr->getCastKind();
-            if (castKind == CK_NullToPointer)
-            {
-                return true;
-            }
-            if (castKind == CK_BitCast)
-            {
-                Expr *subExpr = implicitCastExpr->getSubExpr();
-                while (isa<ParenExpr>(subExpr))
-                {
-                    ParenExpr *parenExpr = dyn_cast<ParenExpr>(subExpr);
-                    subExpr = parenExpr->getSubExpr();
-                }
-                CStyleCastExpr *cStyleCastExpr = dyn_cast<CStyleCastExpr>(subExpr);
-                return cStyleCastExpr && cStyleCastExpr->getCastKind() == CK_NullToPointer;
-            }
-        }
-        return false;
-    }
-
-    bool isImplicitDeclRef(Expr *expr)
-    {
-        ImplicitCastExpr *implicitCastExpr = dyn_cast<ImplicitCastExpr>(expr);
-        return implicitCastExpr &&
-            (implicitCastExpr->getCastKind() == CK_PointerToBoolean ||
-            (implicitCastExpr->getCastKind() == CK_LValueToRValue &&
-            isa<DeclRefExpr>(implicitCastExpr->getSubExpr())));
-    }
-
-    bool isEqNullCheck(Expr *expr)
-    {
-        BinaryOperator *binaryOperator = dyn_cast<BinaryOperator>(expr);
-        if (binaryOperator)
-        {
-            return binaryOperator->getOpcode() == BO_EQ &&
-                isNullToPointer(binaryOperator->getLHS()) !=
-                isNullToPointer(binaryOperator->getRHS());
-        }
-
-        UnaryOperator *unaryOperator = dyn_cast<UnaryOperator>(expr);
-        if (unaryOperator && unaryOperator->getOpcode() == UO_LNot)
-        {
-            return isImplicitDeclRef(unaryOperator->getSubExpr());
-        }
-
-        return false;
-    }
-
-    bool isNeNullCheck(Expr *expr)
-    {
-        BinaryOperator *binaryOperator = dyn_cast<BinaryOperator>(expr);
-        if (binaryOperator)
-        {
-            return binaryOperator->getOpcode() == BO_NE &&
-                isNullToPointer(binaryOperator->getLHS()) !=
-                isNullToPointer(binaryOperator->getRHS());
-        }
-
-        return isImplicitDeclRef(expr);
-    }
 
     bool isEqNullCheckMisplaced(BinaryOperator *binaryOperator)
     {
@@ -87,90 +23,9 @@ private:
         return isEqNullCheckMisplaced(binaryOperator) || isNeNullCheckMisplaced(binaryOperator);
     }
 
-    string extractIdentifierFromImplicitCastExpr(ImplicitCastExpr *implicitCastExpr)
-    {
-        if (!implicitCastExpr)
-        {
-            return "";
-        }
-        if (implicitCastExpr->getCastKind() == CK_PointerToBoolean)
-        {
-            return extractIdentifierFromImplicitCastExpr(
-                dyn_cast<ImplicitCastExpr>(implicitCastExpr->getSubExpr()));
-        }
-        DeclRefExpr *declRefExpr = dyn_cast<DeclRefExpr>(implicitCastExpr->getSubExpr());
-        return declRefExpr ? declRefExpr->getFoundDecl()->getNameAsString() : "";
-    }
-
-    string extractIdentifierFromBinaryOperator(BinaryOperator *binaryOperator)
-    {
-        Expr *refExpr = binaryOperator->getLHS();
-        if (isNullToPointer(refExpr))
-        {
-            refExpr = binaryOperator->getRHS();
-        }
-        return extractIdentifierFromImplicitCastExpr(dyn_cast<ImplicitCastExpr>(refExpr));
-    }
-
-    string extractIdentifierFromUnaryOperator(UnaryOperator *unaryOperator)
-    {
-        return extractIdentifierFromImplicitCastExpr(
-            dyn_cast<ImplicitCastExpr>(unaryOperator->getSubExpr()));
-    }
-
-    string extractIdentifierFromRHS(Expr *expr)
-    {
-        if (isa<ImplicitCastExpr>(expr))
-        {
-            return extractIdentifierFromImplicitCastExpr(dyn_cast<ImplicitCastExpr>(expr));
-        }
-        if (isa<BinaryOperator>(expr))
-        {
-            return extractIdentifierFromBinaryOperator(dyn_cast<BinaryOperator>(expr));
-        }
-        if (isa<UnaryOperator>(expr))
-        {
-            return extractIdentifierFromUnaryOperator(dyn_cast<UnaryOperator>(expr));
-        }
-        return "";
-    }
-
-    class SeekingVariableOfInterest : public RecursiveASTVisitor<SeekingVariableOfInterest>
-    {
-    private:
-        string _variableName;
-        MisplacedNullCheckRule *_rule;
-
-    public:
-        bool hasVariableInExpr(string name, Expr *expr, MisplacedNullCheckRule *rule)
-        {
-            _variableName = name;
-            _rule = rule;
-            return !TraverseStmt(expr);
-        }
-
-        bool VisitMemberExpr(MemberExpr *memberExpr)
-        {
-            if (memberExpr->isArrow())
-            {
-                return _variableName !=
-                    _rule->extractIdentifierFromImplicitCastExpr(
-                    dyn_cast<ImplicitCastExpr>(memberExpr->getBase()));
-            }
-            return true;
-        }
-
-        bool VisitObjCMessageExpr(ObjCMessageExpr *memberExpr)
-        {
-            return _variableName !=
-                _rule->extractIdentifierFromImplicitCastExpr(
-                dyn_cast<ImplicitCastExpr>(memberExpr->getInstanceReceiver()));
-        }
-    };
-
     bool isSameVariableMisplaced(BinaryOperator *binaryOperator)
     {
-        string variableOfInterest = extractIdentifierFromRHS(binaryOperator->getRHS());
+        string variableOfInterest = extractIdentifierFromExpr(binaryOperator->getRHS());
         if (variableOfInterest == "")
         {
             return false;
