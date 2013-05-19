ac03d93e4c06935516b91055dbe5d13559012b77
Longyi Qi <lqi+git@longyiqi.com>
2013-05-19 18:46:16 -0500

    Slightly imporvement to redundant conditional operator rule

--- oclint-rules/rules/redundant/RedundantConditionalOperatorRule.cpp
+++ oclint-rules/rules/redundant/RedundantConditionalOperatorRule.cpp
@@ -155,27 +155,18 @@ public:
 
     bool VisitConditionalOperator(ConditionalOperator *conditionalOperator)
     {
-        // TODO: There are three types of violations: 1. true expression and false expression
-        // are returnning true/false or false/true respectively; 2. true expression and false
+        // There are three types of violations: 1. true expression and false expression
+        // are returning true/false or false/true respectively; 2. true expression and false
         // expression are the same constant; 3. true expression and false expression are the
-        // same variable expression. We are planning to introduce refactoring suggestions in
-        // OCLint v0.8, and we will provide different suggestions to these three cases, then.
-        // e.g., the first case can be replaced by a simple boolean expression, and the
+        // same variable expression.
+        // The first case can be replaced by a simple boolean expression, and the
         // second/third cases can be replaced by constant or variable expression.
 
         Expr *trueExpression = conditionalOperator->getTrueExpr();
         Expr *falseExpression = conditionalOperator->getFalseExpr();
-        if (isNotEquals(trueExpression, falseExpression))
-        {
-            addViolation(conditionalOperator, this);
-        }
-
-        if (isSameConstant(trueExpression, falseExpression))
-        {
-            addViolation(conditionalOperator, this);
-        }
-
-        if (isSameVariable(trueExpression, falseExpression))
+        if (isNotEquals(trueExpression, falseExpression)
+            || isSameConstant(trueExpression, falseExpression)
+            || isSameVariable(trueExpression, falseExpression))
         {
             addViolation(conditionalOperator, this);
         }
