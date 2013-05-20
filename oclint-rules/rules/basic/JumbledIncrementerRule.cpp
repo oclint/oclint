f88241e005a8c27b97295024d6aea62421ef12e6
Longyi Qi <lqi+git@longyiqi.com>
2013-05-20 17:38:32 -0500

    Added jumbled incrementer rule

---
+++ oclint-rules/rules/basic/JumbledIncrementerRule.cpp
@@ -0,0 +1,54 @@
+#include "oclint/AbstractASTVisitorRule.h"
+#include "oclint/RuleSet.h"
+
+class JumbledIncrementerRule : public AbstractASTVisitorRule<JumbledIncrementerRule>
+{
+private:
+    static RuleSet rules;
+
+public:
+    virtual const string name() const
+    {
+        return "jumbled incrementer";
+    }
+
+    virtual int priority() const
+    {
+        return 2;
+    }
+
+    bool VisitForStmt(ForStmt *parentStmt)
+    {
+        Stmt *bodyStmt = parentStmt->getBody();
+        ForStmt *forStmt = dyn_cast<ForStmt>(bodyStmt);
+        CompoundStmt *compoundStmt = dyn_cast<CompoundStmt>(bodyStmt);
+        if (!forStmt && compoundStmt && compoundStmt->size() == 1)
+        {
+            forStmt = dyn_cast<ForStmt>(compoundStmt->body_back());
+        }
+        if (forStmt)
+        {
+            Stmt *initStmt = parentStmt->getInit();
+            Expr *incExpr = forStmt->getInc();
+            if (initStmt && incExpr)
+            {
+                DeclStmt *declStmt = dyn_cast<DeclStmt>(initStmt);
+                UnaryOperator *unaryOperator = dyn_cast<UnaryOperator>(incExpr);
+                if (declStmt && unaryOperator &&
+                    declStmt->isSingleDecl() && isa<DeclRefExpr>(unaryOperator->getSubExpr()))
+                {
+                    VarDecl *varDecl = dyn_cast<VarDecl>(declStmt->getSingleDecl());
+                    DeclRefExpr *declRefExpr = dyn_cast<DeclRefExpr>(unaryOperator->getSubExpr());
+                    ValueDecl *valueDecl = declRefExpr->getDecl();
+                    if (varDecl && valueDecl && varDecl == valueDecl)
+                    {
+                        addViolation(incExpr, this);
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
+
+RuleSet JumbledIncrementerRule::rules(new JumbledIncrementerRule());
