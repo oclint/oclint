68378b16a700732c23c9deaff04d640afae3f249
Longyi Qi <lqi+git@longyiqi.com>
2013-05-26 18:14:38 -0500

    Modify rules that use metrics

--- oclint-rules/test/size/NPathComplexityRuleTest.cpp
+++ oclint-rules/test/size/NPathComplexityRuleTest.cpp
@@ -6,7 +6,7 @@ class NPathComplexityRuleTest : public ::testing::Test {
 protected:
     virtual void SetUp()
     {
-        RuleConfiguration::addConfiguration("NPATH_COMPLEXITY", "0");
+        RuleConfiguration::addConfiguration("NPATH_COMPLEXITY", "1");
     }
 
     virtual void TearDown()
@@ -24,128 +24,127 @@ TEST_F(NPathComplexityRuleTest, PropertyTest)
 
 TEST_F(NPathComplexityRuleTest, EmptyMethod)
 {
-    testRuleOnCode(new NPathComplexityRule(), "void mthd() { }",
-        0, 1, 1, 1, 15, "NPath Complexity Number 1 exceeds limit of 0");
+    testRuleOnCode(new NPathComplexityRule(), "void mthd() { }");
 }
 
 TEST_F(NPathComplexityRuleTest, IfStmtWithConstantConditionAndEmptyThenCompoundStmt)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { if (1) {} }",
-        0, 1, 1, 1, 25, "NPath Complexity Number 2 exceeds limit of 0");
+        0, 1, 1, 1, 25, "NPath Complexity Number 2 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, IfStmtWithOneLogicAndOperatorConditionAndEmptyThenCompoundStmt)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { if (1 && 0) {} }",
-        0, 1, 1, 1, 30, "NPath Complexity Number 3 exceeds limit of 0");
+        0, 1, 1, 1, 30, "NPath Complexity Number 3 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, IfStmtWithOneLogicOrOperatorConditionAndEmptyThenCompoundStmt)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { if (1 || 0) {} }",
-        0, 1, 1, 1, 30, "NPath Complexity Number 3 exceeds limit of 0");
+        0, 1, 1, 1, 30, "NPath Complexity Number 3 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, IfStmtWithMultipleLogicAndOrOperatorsConditionAndEmptyThenCompoundStmt)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { if (0 || 1 && 0) {} }",
-        0, 1, 1, 1, 35, "NPath Complexity Number 4 exceeds limit of 0");
+        0, 1, 1, 1, 35, "NPath Complexity Number 4 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, IfElseStmtWithConstantConditionAndEmptyThenCompoundStmt)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { if (1) {} else {} }",
-        0, 1, 1, 1, 33, "NPath Complexity Number 2 exceeds limit of 0");
+        0, 1, 1, 1, 33, "NPath Complexity Number 2 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, IfElseStmtWithOneLogicAndOperatorConditionAndEmptyThenCompoundStmt)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { if (1 && 0) {} else {} }",
-        0, 1, 1, 1, 38, "NPath Complexity Number 3 exceeds limit of 0");
+        0, 1, 1, 1, 38, "NPath Complexity Number 3 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, IfElseStmtWithOneLogicOrOperatorConditionAndEmptyThenCompoundStmt)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { if (1 || 0) {} else {} }",
-        0, 1, 1, 1, 38, "NPath Complexity Number 3 exceeds limit of 0");
+        0, 1, 1, 1, 38, "NPath Complexity Number 3 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, IfElseStmtWithMultipleLogicAndOrOperatorsConditionAndEmptyThenCompoundStmt)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { if (0 || 1 && 0) {} else {} }",
-        0, 1, 1, 1, 43, "NPath Complexity Number 4 exceeds limit of 0");
+        0, 1, 1, 1, 43, "NPath Complexity Number 4 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, NestedIfStmt)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { if (1) { if (1) {} } }",
-        0, 1, 1, 1, 36, "NPath Complexity Number 3 exceeds limit of 0");
+        0, 1, 1, 1, 36, "NPath Complexity Number 3 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, NestedIfElseStmt)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { if (1) { if (1) {} else {} } else { if (1) {} else {} } }",
-        0, 1, 1, 1, 71, "NPath Complexity Number 4 exceeds limit of 0");
+        0, 1, 1, 1, 71, "NPath Complexity Number 4 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, WhileStmtWithConstantConditionAndEmptyCompoundStmt)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { while (1) {} }",
-        0, 1, 1, 1, 28, "NPath Complexity Number 2 exceeds limit of 0");
+        0, 1, 1, 1, 28, "NPath Complexity Number 2 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, WhileStmtWithOneLogicAndOperatorConditionAndEmptyCompoundStmt)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { while (1 && 0) {} }",
-        0, 1, 1, 1, 33, "NPath Complexity Number 3 exceeds limit of 0");
+        0, 1, 1, 1, 33, "NPath Complexity Number 3 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, WhileStmtWithOneLogicOrOperatorConditionAndEmptyCompoundStmt)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { while (1 || 0) {} }",
-        0, 1, 1, 1, 33, "NPath Complexity Number 3 exceeds limit of 0");
+        0, 1, 1, 1, 33, "NPath Complexity Number 3 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, DoWhileStmtWithConstantConditionAndEmptyCompoundStmt)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { do {} while (1); }",
-        0, 1, 1, 1, 32, "NPath Complexity Number 2 exceeds limit of 0");
+        0, 1, 1, 1, 32, "NPath Complexity Number 2 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, DoWhileStmtWithOneLogicAndOperatorConditionAndEmptyCompoundStmt)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { do {} while (1 && 0); }",
-        0, 1, 1, 1, 37, "NPath Complexity Number 3 exceeds limit of 0");
+        0, 1, 1, 1, 37, "NPath Complexity Number 3 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, DoWhileStmtWithOneLogicOrOperatorConditionAndEmptyCompoundStmt)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { do {} while (1 || 0); }",
-        0, 1, 1, 1, 37, "NPath Complexity Number 3 exceeds limit of 0");
+        0, 1, 1, 1, 37, "NPath Complexity Number 3 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, ForStmtWithSimpleConditionAndEmptyCompoundStmt)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { for (int i = 0; i < 1; i++) {} }",
-        0, 1, 1, 1, 46, "NPath Complexity Number 3 exceeds limit of 0");
+        0, 1, 1, 1, 46, "NPath Complexity Number 3 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, ForStmtWithOneLogicAndOperatorConditionAndEmptyCompoundStmt)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { for (int i = 0; i < 1 && 0; i++) {} }",
-        0, 1, 1, 1, 51, "NPath Complexity Number 4 exceeds limit of 0");
+        0, 1, 1, 1, 51, "NPath Complexity Number 4 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, ObjCForeachStmtWithEmptyCompoundStmt)
 {
     testRuleOnObjCCode(new NPathComplexityRule(), "void mthd() { id anArray; for (id it in anArray) {} }",
-        0, 1, 1, 1, 53, "NPath Complexity Number 3 exceeds limit of 0");
+        0, 1, 1, 1, 53, "NPath Complexity Number 3 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, ObjCForeachStmtWithCompoundStmtWithIfStmt)
 {
     testRuleOnObjCCode(new NPathComplexityRule(), "void mthd() { id anArray; for (id it in anArray) { if (1) {} } }",
-        0, 1, 1, 1, 64, "NPath Complexity Number 4 exceeds limit of 0");
+        0, 1, 1, 1, 64, "NPath Complexity Number 4 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, SwitchStmtWithSimpleConditionAndEmptyBody)
@@ -155,55 +154,54 @@ TEST_F(NPathComplexityRuleTest, SwitchStmtWithSimpleConditionAndEmptyBody)
 
 TEST_F(NPathComplexityRuleTest, SwitchStmtWithSimpleConditionAndOneCase)
 {
-    testRuleOnCode(new NPathComplexityRule(), "void mthd() { int i; switch (i) { case 1: break; } }",
-        0, 1, 1, 1, 52, "NPath Complexity Number 1 exceeds limit of 0");
+    testRuleOnCode(new NPathComplexityRule(), "void mthd() { int i; switch (i) { case 1: break; } }");
 }
 
 TEST_F(NPathComplexityRuleTest, SwitchStmtWithSimpleConditionAndOneCaseOneDefault)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { int i; switch (i) { case 1: break; default: break; } }",
-        0, 1, 1, 1, 68, "NPath Complexity Number 2 exceeds limit of 0");
+        0, 1, 1, 1, 68, "NPath Complexity Number 2 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, SwitchStmtWithSimpleConditionAndTwoCasesOneDefault)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { int i; switch (i) { case 1: break; case 2: break; default: break; } }",
-        0, 1, 1, 1, 83, "NPath Complexity Number 3 exceeds limit of 0");
+        0, 1, 1, 1, 83, "NPath Complexity Number 3 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, SwitchStmtWithSimpleConditionAndOneCaseOneIfOneDefault)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { int i; switch (i) { case 1: break; if (i > 0 && i < 0) {} default: break; } }",
-        0, 1, 1, 1, 91, "NPath Complexity Number 4 exceeds limit of 0");
+        0, 1, 1, 1, 91, "NPath Complexity Number 4 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, SwitchStmtWithSimpleConditionAndOneCaseAsIfOneDefault)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { int i; switch (i) { case 1: if (i > 0 && i < 0) {} break; default: break; } }",
-        0, 1, 1, 1, 91, "NPath Complexity Number 4 exceeds limit of 0");
+        0, 1, 1, 1, 91, "NPath Complexity Number 4 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, SwitchStmtWithSimpleConditionAndOneCaseAsTwoIfsOneDefault)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { int i; switch (i) { case 1: if (i > 0) {} if (i < 0) {} break; default: break; } }",
-        0, 1, 1, 1, 96, "NPath Complexity Number 5 exceeds limit of 0");
+        0, 1, 1, 1, 96, "NPath Complexity Number 5 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, BasicConditionalOperator)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { if (1 ? 2 : 3) {} }",
-        0, 1, 1, 1, 33, "NPath Complexity Number 4 exceeds limit of 0");
+        0, 1, 1, 1, 33, "NPath Complexity Number 4 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, NastingConditionalOperator)
 {
     testRuleOnCode(new NPathComplexityRule(), "void mthd() { if (1 ? (2 ? 3 : 4) : (5 ? 6 : 7)) {} }",
-        0, 1, 1, 1, 53, "NPath Complexity Number 8 exceeds limit of 0");
+        0, 1, 1, 1, 53, "NPath Complexity Number 8 exceeds limit of 1");
 }
 
 TEST_F(NPathComplexityRuleTest, SuppressHighNPathComplexity)
 {
-    testRuleOnCode(new NPathComplexityRule(), "void __attribute__((annotate(\"oclint:suppress[high npath complexity]\"))) mthd() { }");
+    testRuleOnCode(new NPathComplexityRule(), "void __attribute__((annotate(\"oclint:suppress[high npath complexity]\"))) mthd() { if (1 ? 2 : 3) {} }");
 }
 
 int main(int argc, char **argv)
--- oclint-rules/test/size/NcssMethodCountRuleTest.cpp
+++ oclint-rules/test/size/NcssMethodCountRuleTest.cpp
@@ -6,7 +6,7 @@ class NcssMethodCountRuleTest : public ::testing::Test {
 protected:
     virtual void SetUp()
     {
-        RuleConfiguration::addConfiguration("NCSS_METHOD", "0");
+        RuleConfiguration::addConfiguration("NCSS_METHOD", "1");
     }
 
     virtual void TearDown()
@@ -24,8 +24,7 @@ TEST_F(NcssMethodCountRuleTest, PropertyTest)
 
 TEST_F(NcssMethodCountRuleTest, EmptyFunction)
 {
-    testRuleOnCode(new NcssMethodCountRule(), "void aMethod() { }",
-        0, 1, 1, 1, 18, "Method of 1 non-commenting source statements exceeds limit of 0");
+    testRuleOnCode(new NcssMethodCountRule(), "void aMethod() { }");
 }
 
 TEST_F(NcssMethodCountRuleTest, NullStmtsDoNotContribute)
@@ -35,26 +34,25 @@ TEST_F(NcssMethodCountRuleTest, NullStmtsDoNotContribute)
 ;\n\
 ;\n\
 ;\n\
-;}",
-        0, 1, 1, 6, 2, "Method of 1 non-commenting source statements exceeds limit of 0");
+;}");
 }
 
 TEST_F(NcssMethodCountRuleTest, SemiColonAfterDeclStmtCount)
 {
     testRuleOnCode(new NcssMethodCountRule(), "void aMethod() { int a = 1; }",
-        0, 1, 1, 1, 29, "Method of 2 non-commenting source statements exceeds limit of 0");
+        0, 1, 1, 1, 29, "Method of 2 non-commenting source statements exceeds limit of 1");
 }
 
 TEST_F(NcssMethodCountRuleTest, DeclStmtWithManyVariables)
 {
     testRuleOnCode(new NcssMethodCountRule(), "void m() { int a = 1, b = 2, c = 3; }",
-        0, 1, 1, 1, 37, "Method of 2 non-commenting source statements exceeds limit of 0");
+        0, 1, 1, 1, 37, "Method of 2 non-commenting source statements exceeds limit of 1");
 }
 
 TEST_F(NcssMethodCountRuleTest, MethodCall)
 {
     testRuleOnCode(new NcssMethodCountRule(), "void n(); void m() { n(); }",
-        0, 1, 11, 1, 27, "Method of 2 non-commenting source statements exceeds limit of 0");
+        0, 1, 11, 1, 27, "Method of 2 non-commenting source statements exceeds limit of 1");
 }
 
 TEST_F(NcssMethodCountRuleTest, EmptyCompoundStmt)
@@ -64,97 +62,96 @@ TEST_F(NcssMethodCountRuleTest, EmptyCompoundStmt)
 {}\n\
 {}\n\
 {}\n\
-{}}",
-        0, 1, 1, 6, 3, "Method of 1 non-commenting source statements exceeds limit of 0");
+{}}");
 }
 
 TEST_F(NcssMethodCountRuleTest, IfStatement)
 {
     testRuleOnCode(new NcssMethodCountRule(), "void m() { if (1) {} }",
-        0, 1, 1, 1, 22, "Method of 2 non-commenting source statements exceeds limit of 0");
+        0, 1, 1, 1, 22, "Method of 2 non-commenting source statements exceeds limit of 1");
 }
 
 TEST_F(NcssMethodCountRuleTest, IfStatementWithElse)
 {
     testRuleOnCode(new NcssMethodCountRule(), "void m() { if (1) {} else {} }",
-        0, 1, 1, 1, 30, "Method of 3 non-commenting source statements exceeds limit of 0");
+        0, 1, 1, 1, 30, "Method of 3 non-commenting source statements exceeds limit of 1");
 }
 
 TEST_F(NcssMethodCountRuleTest, ForStatement)
 {
     testRuleOnCode(new NcssMethodCountRule(), "void m() { for(;;) {} }",
-        0, 1, 1, 1, 23, "Method of 2 non-commenting source statements exceeds limit of 0");
+        0, 1, 1, 1, 23, "Method of 2 non-commenting source statements exceeds limit of 1");
 }
 
 TEST_F(NcssMethodCountRuleTest, ObjCForCollectionStatement)
 {
     testRuleOnObjCCode(new NcssMethodCountRule(), "void m() { id array; for(id one in array) {} }",
-        0, 1, 1, 1, 46, "Method of 3 non-commenting source statements exceeds limit of 0");
+        0, 1, 1, 1, 46, "Method of 3 non-commenting source statements exceeds limit of 1");
 }
 
 TEST_F(NcssMethodCountRuleTest, WhileStatement)
 {
     testRuleOnCode(new NcssMethodCountRule(), "void m() { while(1) {} }",
-        0, 1, 1, 1, 24, "Method of 2 non-commenting source statements exceeds limit of 0");
+        0, 1, 1, 1, 24, "Method of 2 non-commenting source statements exceeds limit of 1");
 }
 
 TEST_F(NcssMethodCountRuleTest, DoStatement)
 {
     testRuleOnCode(new NcssMethodCountRule(), "void m() { do {} while(1); }",
-        0, 1, 1, 1, 28, "Method of 3 non-commenting source statements exceeds limit of 0");
+        0, 1, 1, 1, 28, "Method of 3 non-commenting source statements exceeds limit of 1");
 }
 
 TEST_F(NcssMethodCountRuleTest, CXXTryCatch)
 {
     testRuleOnCXXCode(new NcssMethodCountRule(), "void m() { try { int a = 1; } catch (...) { int b = 2; } }",
-        0, 1, 1, 1, 58, "Method of 5 non-commenting source statements exceeds limit of 0");
+        0, 1, 1, 1, 58, "Method of 5 non-commenting source statements exceeds limit of 1");
 }
 
 TEST_F(NcssMethodCountRuleTest, ObjCTryCatch)
 {
     testRuleOnObjCCode(new NcssMethodCountRule(), "void m() { @try {} @catch (id ex) {} }",
-        0, 1, 1, 1, 38, "Method of 3 non-commenting source statements exceeds limit of 0");
+        0, 1, 1, 1, 38, "Method of 3 non-commenting source statements exceeds limit of 1");
 }
 
 TEST_F(NcssMethodCountRuleTest, ObjCTryCatchFinally)
 {
     testRuleOnObjCCode(new NcssMethodCountRule(), "void m() { @try { int c = 3; } @catch (id ex) { int a = 1; } @finally { int b = 2; } }",
-        0, 1, 1, 1, 86, "Method of 7 non-commenting source statements exceeds limit of 0");
+        0, 1, 1, 1, 86, "Method of 7 non-commenting source statements exceeds limit of 1");
 }
 
 TEST_F(NcssMethodCountRuleTest, ObjCAutoreleasePool)
 {
     testRuleOnObjCCode(new NcssMethodCountRule(), "void m() { @autoreleasepool { int a = 1; } }",
-        0, 1, 1, 1, 44, "Method of 3 non-commenting source statements exceeds limit of 0");
+        0, 1, 1, 1, 44, "Method of 3 non-commenting source statements exceeds limit of 1");
 }
 
 TEST_F(NcssMethodCountRuleTest, ObjCSynchronized)
 {
     testRuleOnObjCCode(new NcssMethodCountRule(), "void m() { id res; @synchronized(res) { int a = 1; } }",
-        0, 1, 1, 1, 54, "Method of 4 non-commenting source statements exceeds limit of 0");
+        0, 1, 1, 1, 54, "Method of 4 non-commenting source statements exceeds limit of 1");
 }
 
 TEST_F(NcssMethodCountRuleTest, OneCaseStatement)
 {
     testRuleOnCode(new NcssMethodCountRule(), "void m() { int i = 1; switch (i) { case 1: break; } }",
-        0, 1, 1, 1, 53, "Method of 5 non-commenting source statements exceeds limit of 0");
+        0, 1, 1, 1, 53, "Method of 5 non-commenting source statements exceeds limit of 1");
 }
 
 TEST_F(NcssMethodCountRuleTest, TwoCaseStatements)
 {
     testRuleOnCode(new NcssMethodCountRule(), "void m() { int i = 1; switch (i) { case 1: case 2: break; } }",
-        0, 1, 1, 1, 61, "Method of 6 non-commenting source statements exceeds limit of 0");
+        0, 1, 1, 1, 61, "Method of 6 non-commenting source statements exceeds limit of 1");
 }
 
 TEST_F(NcssMethodCountRuleTest, TwoCaseStatementsAndDefault)
 {
     testRuleOnCode(new NcssMethodCountRule(), "void m() { int i = 1; switch (i) { case 1: i = 2; break; case 2: break; default: break; } }",
-        0, 1, 1, 1, 91, "Method of 10 non-commenting source statements exceeds limit of 0");
+        0, 1, 1, 1, 91, "Method of 10 non-commenting source statements exceeds limit of 1");
 }
 
 TEST_F(NcssMethodCountRuleTest, SupressHighNcssMethod)
 {
-    testRuleOnCode(new NcssMethodCountRule(), "void __attribute__((annotate(\"oclint:suppress[high ncss method]\"))) aMethod() { }");
+    testRuleOnCode(new NcssMethodCountRule(), "void __attribute__((annotate(\"oclint:suppress[high ncss method]\"))) aMethod() { do {} while(1); }");
 }
 
 int main(int argc, char **argv)
