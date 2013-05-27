491063377383e9ddb05d6ecec8a6eff293517458
Longyi Qi <lqi+git@longyiqi.com>
2013-05-27 08:12:17 -0500

    pump to 0.8dev

--- oclint-core/test/VersionTest.cpp
+++ oclint-core/test/VersionTest.cpp
@@ -7,7 +7,7 @@ using namespace ::testing;
 
 TEST(VersionTest, VersionString)
 {
-    EXPECT_THAT(Version::identifier(), StrEq("0.7dev"));
+    EXPECT_THAT(Version::identifier(), StrEq("0.8dev"));
 }
 
 int main(int argc, char **argv)
