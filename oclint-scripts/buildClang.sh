a4c75a8853ba714617618367cc23ed82d9869c1c
Martin Olsson <martin@minimum.se>
2013-05-29 17:01:30 +0200

    Use all CPU cores to build clang
    Fixes oclint github issue #38.

--- oclint-scripts/buildClang.sh
+++ oclint-scripts/buildClang.sh
@@ -24,9 +24,21 @@ fi
 mkdir -p $LLVM_BUILD
 cd $LLVM_BUILD
 
+if [ "$CPU_CORES" = "" ]; then
+    # Default to building on all cores, but allow single core builds using:
+    # "CPU_CORES=1 ./buildClang.sh release"
+    CPU_CORES=1
+    OS=$(uname -s)
+    if [ "$OS" = "Linux" ]; then
+        CPU_CORES=$(grep -c ^processor /proc/cpuinfo)
+    elif [ "$OS" = "Darwin" ]; then
+        CPU_CORES=$(sysctl -n hw.ncpu)
+    fi
+fi
+
 # configure and build
 cmake $RELEASE_CONFIG -D CMAKE_INSTALL_PREFIX=$LLVM_INSTALL $LLVM_SRC
-make
+make -j $CPU_CORES
 make install
 
 # back to the current folder
