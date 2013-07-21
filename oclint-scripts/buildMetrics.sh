4901fa2e143e48387d13769fb4a785a7b2c9ca33
Joris <joris.dauphin+git@gmail.com>
2013-07-21 16:01:50 +0200

    Add support of MinGW in some scripts. (and support spaces in some path)

--- oclint-scripts/buildMetrics.sh
+++ oclint-scripts/buildMetrics.sh
@@ -8,9 +8,21 @@ LLVM_BUILD="$PROJECT_ROOT/build/llvm-install"
 OCLINT_METRICS_SRC="$PROJECT_ROOT/oclint-metrics"
 OCLINT_METRICS_BUILD="$PROJECT_ROOT/build/oclint-metrics"
 
+EXTRA="-D CMAKE_CXX_COMPILER=$LLVM_BUILD/bin/clang++ -D CMAKE_C_COMPILER=$LLVM_BUILD/bin/clang"
+GENERATOR_FLAG=
+GENERATOR=
+
+OS=$(uname -s)
+if [[ "$OS" =~ MINGW32 ]]; then
+    GENERATOR_FLAG="-G"
+    GENERATOR="MSYS Makefiles"
+    # use default compiler (g++)
+    EXTRA=
+fi
+
 # clean test directory
 if [ $# -eq 1 ] && [ "$1" = "clean" ]; then
-    rm -rf $OCLINT_METRICS_BUILD
+    rm -rf "$OCLINT_METRICS_BUILD"
     exit 0
 fi
 
@@ -21,12 +33,12 @@ if [ $# -eq 1 ] && [ "$1" = "release" ]; then
 fi
 
 # create directory and prepare for build
-mkdir -p $OCLINT_METRICS_BUILD
-cd $OCLINT_METRICS_BUILD
+mkdir -p "$OCLINT_METRICS_BUILD"
+cd "$OCLINT_METRICS_BUILD"
 
 # configure and build
-cmake $RELEASE_CONFIG -D CMAKE_CXX_COMPILER=$LLVM_BUILD/bin/clang++ -D CMAKE_C_COMPILER=$LLVM_BUILD/bin/clang -D LLVM_ROOT=$LLVM_BUILD $OCLINT_METRICS_SRC
+cmake $RELEASE_CONFIG $EXTRA -D LLVM_ROOT="$LLVM_BUILD" "$OCLINT_METRICS_SRC" "$GENERATOR_FLAG" "$GENERATOR"
 make
 
 # back to the current folder
-cd $CWD
+cd "$CWD"
--- oclint-scripts/buildRules.sh
+++ oclint-scripts/buildRules.sh
@@ -3,7 +3,7 @@
 # setup environment variables
 CWD=`pwd`
 PROJECT_ROOT="$CWD/.."
-LLVM_SRC="$PROJECT_ROOT/llvm"
+#LLVM_SRC="$PROJECT_ROOT/llvm"
 LLVM_BUILD="$PROJECT_ROOT/build/llvm-install"
 OCLINT_CORE_SRC="$PROJECT_ROOT/oclint-core"
 OCLINT_CORE_BUILD="$PROJECT_ROOT/build/oclint-core"
@@ -11,6 +11,18 @@ OCLINT_METRICS_SRC="$PROJECT_ROOT/oclint-metrics"
 OCLINT_METRICS_BUILD="$PROJECT_ROOT/build/oclint-metrics"
 OCLINT_RULES_SRC="$PROJECT_ROOT/oclint-rules"
 OCLINT_RULES_BUILD="$PROJECT_ROOT/build/oclint-rules"
+
+EXTRA="-D CMAKE_CXX_COMPILER=$LLVM_BUILD/bin/clang++ -D CMAKE_C_COMPILER=$LLVM_BUILD/bin/clang"
+GENERATOR_FLAG=
+GENERATOR=
+
+OS=$(uname -s)
+if [[ "$OS" =~ MINGW32 ]]; then
+    GENERATOR_FLAG="-G"
+    GENERATOR="MSYS Makefiles"
+    # use default compiler (g++)
+    EXTRA=
+fi
 
 # clean test directory
 if [ $# -eq 1 ] && [ "$1" = "clean" ]; then
@@ -25,12 +37,12 @@ if [ $# -eq 1 ] && [ "$1" = "release" ]; then
 fi
 
 # create directory and prepare for build
-mkdir -p $OCLINT_RULES_BUILD
-cd $OCLINT_RULES_BUILD
+mkdir -p "$OCLINT_RULES_BUILD"
+cd "$OCLINT_RULES_BUILD"
 
 # configure and build
-cmake $RELEASE_CONFIG -D CMAKE_CXX_COMPILER=$LLVM_BUILD/bin/clang++ -D CMAKE_C_COMPILER=$LLVM_BUILD/bin/clang -D LLVM_ROOT=$LLVM_BUILD -D OCLINT_SOURCE_DIR=$OCLINT_CORE_SRC -D OCLINT_BUILD_DIR=$OCLINT_CORE_BUILD -D OCLINT_METRICS_SOURCE_DIR=$OCLINT_METRICS_SRC -D OCLINT_METRICS_BUILD_DIR=$OCLINT_METRICS_BUILD $OCLINT_RULES_SRC
+cmake $RELEASE_CONFIG $EXTRA -D LLVM_ROOT="$LLVM_BUILD" -D OCLINT_SOURCE_DIR="$OCLINT_CORE_SRC" -D OCLINT_BUILD_DIR="$OCLINT_CORE_BUILD" -D OCLINT_METRICS_SOURCE_DIR="$OCLINT_METRICS_SRC" -D OCLINT_METRICS_BUILD_DIR="$OCLINT_METRICS_BUILD" "$OCLINT_RULES_SRC" "$GENERATOR_FLAG" "$GENERATOR"
 make
 
 # back to the current folder
-cd $CWD
+cd "$CWD"
