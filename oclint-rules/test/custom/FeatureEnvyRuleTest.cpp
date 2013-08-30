eb8b213d12d2b780b460e41ae09fc794b0c8ad98
Joseph Kain <joekain@gmail.com>
2013-08-29 20:40:23 -0700

    Remove unnecessary test fixtures from FeatureEnvy

--- oclint-rules/test/custom/FeatureEnvyRuleTest.cpp
+++ oclint-rules/test/custom/FeatureEnvyRuleTest.cpp
@@ -2,32 +2,19 @@
 
 #include "rules/custom/FeatureEnvyRule.cpp"
 
-class FeatureEnvyRuleTest : public ::testing::Test {
-protected:
-    virtual void SetUp()
-    {
-        RuleConfiguration::addConfiguration("FEATURE_ENVY", "3");
-    }
-
-    virtual void TearDown()
-    {
-        RuleConfiguration::removeAll();
-    }
-};
-
-TEST_F(FeatureEnvyRuleTest, PropertyTest)
+TEST(FeatureEnvyRuleTest, PropertyTest)
 {
     FeatureEnvyRule rule;
     EXPECT_EQ(3, rule.priority());
     EXPECT_EQ("feature envy", rule.name());
 }
 
-TEST_F(FeatureEnvyRuleTest, ObjCImplementationNoMethod)
+TEST(FeatureEnvyRuleTest, ObjCImplementationNoMethod)
 {
   testRuleOnObjCCode(new FeatureEnvyRule(), "@interface a\n@end\n@implementation a\n@end\n");
 }
 
-TEST_F(FeatureEnvyRuleTest, ObjcMethodMessageOther)
+TEST(FeatureEnvyRuleTest, ObjcMethodMessageOther)
 {
     testRuleOnObjCCode(new FeatureEnvyRule(),
       "@interface B \n"
