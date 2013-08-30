7e060b12b0eb20242a7eeeedb198bd90f6f883e0
Joseph Kain <joekain@gmail.com>
2013-08-29 20:30:39 -0700

    Implement FeatureEnvy
    For each ObjC method scan for message expressions.  Count the number of
    messages objects of each class receive.  Self should be messaged most else
    the class envies another.

--- oclint-rules/test/custom/FeatureEnvyRuleTest.cpp
+++ oclint-rules/test/custom/FeatureEnvyRuleTest.cpp
@@ -6,7 +6,7 @@ class FeatureEnvyRuleTest : public ::testing::Test {
 protected:
     virtual void SetUp()
     {
-        RuleConfiguration::addConfiguration("TOO_MANY_METHODS", "3");
+        RuleConfiguration::addConfiguration("FEATURE_ENVY", "3");
     }
 
     virtual void TearDown()
@@ -20,6 +20,34 @@ TEST_F(FeatureEnvyRuleTest, PropertyTest)
     FeatureEnvyRule rule;
     EXPECT_EQ(3, rule.priority());
     EXPECT_EQ("feature envy", rule.name());
+}
+
+TEST_F(FeatureEnvyRuleTest, ObjCImplementationNoMethod)
+{
+  testRuleOnObjCCode(new FeatureEnvyRule(), "@interface a\n@end\n@implementation a\n@end\n");
+}
+
+TEST_F(FeatureEnvyRuleTest, ObjcMethodMessageOther)
+{
+    testRuleOnObjCCode(new FeatureEnvyRule(),
+      "@interface B \n"
+      "@end \n"
+
+      "@implementation B \n"
+      "- (void) f { \n"
+      "} \n"
+      "@end \n"
+
+      "@interface A \n"
+      "@end \n"
+
+      "@implementation A \n"
+      "- (void)f { \n"
+      "    B *b; \n"
+      "    [b f]; \n"
+      "} \n"
+      "@end\n",
+      0, 10, 1, 13, 1, "Method f messages B more than self.");
 }
 
 int main(int argc, char **argv)
