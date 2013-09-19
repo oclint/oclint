e3e2e2accf55bc7954a0c9008efeb7848933b6e4
Jarod42 <joris.dauphin@gmail.com>
2013-09-19 15:34:12 +0200

    Add new rule DeleteNullptrIsSafeRule. Add testRuleOnCXX11Code to test c++11 code.

---
+++ oclint-rules/rules/redundant/DeleteNullptrIsSafeRule.cpp
@@ -0,0 +1,154 @@
+#include "oclint/AbstractASTVisitorRule.h"
+#include "oclint/RuleSet.h"
+
+using namespace clang;
+
+static bool IsANullPointer(const Expr& expr)
+{
+    for (const CastExpr* castExpr = dyn_cast<CastExpr>(&expr);
+         castExpr;
+         castExpr = dyn_cast<CastExpr>(castExpr->getSubExpr())) {
+        if (castExpr->getCastKind() == CK_NullToPointer) {
+            return true;
+        }
+    }
+    return false;
+}
+
+static bool AreSameExpr(ASTContext& context, const Expr& lhs, const Expr& rhs)
+{
+    llvm::FoldingSetNodeID lhsID;
+    llvm::FoldingSetNodeID rhsID;
+    lhs.Profile(lhsID, context, true);
+    rhs.Profile(rhsID, context, true);
+    return lhsID == rhsID;
+}
+
+static const Expr* IgnoreCastExpr(const Expr& expr)
+{
+    const Expr* last = &expr;
+    for (const CastExpr* e = dyn_cast<CastExpr>(&expr);
+         e;
+         e = dyn_cast<CastExpr>(e->getSubExpr())) {
+        last = e->getSubExpr();
+    }
+    return last;
+}
+
+static bool IsCondNoNullPointer_binop(const BinaryOperator& b, const Expr** pointer)
+{
+    if (b.getOpcode() != BO_NE) {
+        return false;
+    }
+    const Expr& lhs = *b.getLHS();
+    const Expr& rhs = *b.getRHS();
+    *pointer = nullptr;
+    if (IsANullPointer(lhs) == true) {
+        *pointer = IgnoreCastExpr(rhs);
+    } else if (IsANullPointer(rhs) == true) {
+        *pointer = IgnoreCastExpr(lhs);
+    }
+    return *pointer != nullptr;
+}
+
+static bool IsCondNoNullPointer(const Expr& expr, const Expr** pointer)
+{
+    const BinaryOperator* binop = dyn_cast<BinaryOperator>(&expr);
+    if (binop) {
+        return IsCondNoNullPointer_binop(*binop, pointer);
+    }
+    *pointer = IgnoreCastExpr(expr);
+    return *pointer != nullptr;
+}
+
+static bool IsADeleteStmt(ASTContext& context, const Stmt& stmt, const Expr& pointer)
+{
+    const CXXDeleteExpr* cxxDeleteExpr = dyn_cast<CXXDeleteExpr>(&stmt);
+
+    if (cxxDeleteExpr == nullptr) {
+        return false;
+    }
+    const Expr* expr = IgnoreCastExpr(*cxxDeleteExpr->getArgument());
+    if (expr == nullptr) {
+        return false;
+    }
+    return AreSameExpr(context, *expr, pointer);
+}
+
+static bool IsAnAssignToNullptr(ASTContext& context, const Stmt& stmt, const Expr& pointer)
+{
+    const BinaryOperator* binOp = dyn_cast<BinaryOperator>(&stmt);
+
+    if (binOp == nullptr || binOp->getOpcode() != BO_Assign) {
+        return false;
+    }
+    const Expr& lhs = *IgnoreCastExpr(*binOp->getLHS());
+
+    return IsANullPointer(*binOp->getRHS()) && AreSameExpr(context, lhs, pointer);
+}
+
+static bool IsADeleteBlock_compound(ASTContext& context, const CompoundStmt& compoundStmt, const Expr& pointer)
+{
+    if (compoundStmt.size() != 1 && compoundStmt.size() != 2) {
+        return false;
+    }
+    const Stmt* stmt1 = *compoundStmt.body_begin();
+
+    if (IsADeleteStmt(context, *stmt1, pointer) == false) {
+        return false;
+    }
+    if (compoundStmt.size() == 1) {
+        return true;
+    }
+    const Stmt* stmt2 = *(compoundStmt.body_begin() + 1);
+    return IsAnAssignToNullptr(context, *stmt2, pointer);
+}
+
+static bool IsADeleteBlock(ASTContext& context, const Stmt& stmt, const Expr* pointer)
+{
+    if (pointer == nullptr) {
+        return false;
+    }
+    const CompoundStmt* compoundStmt = dyn_cast<CompoundStmt>(&stmt);
+    if (compoundStmt) {
+        return IsADeleteBlock_compound(context, *compoundStmt, *pointer);
+    }
+    return IsADeleteStmt(context, stmt, *pointer);
+}
+
+class DeleteNullptrIsSafeRule : public oclint::AbstractASTVisitorRule<DeleteNullptrIsSafeRule>
+{
+private:
+    static oclint::RuleSet rules;
+
+public:
+    virtual std::string name() const
+    {
+        return "delete nullptr is safe";
+    }
+
+    virtual int priority() const
+    {
+        return 3;
+    }
+
+    bool VisitIfStmt(IfStmt* ifStmt)
+    {
+        if (ifStmt == nullptr || ifStmt->getElse() != nullptr) {
+            return true;
+        }
+        const Expr* pointer = nullptr;
+        if (IsCondNoNullPointer(*ifStmt->getCond(), &pointer) == false) {
+            return true;
+        }
+        ASTContext* context = _carrier->getASTContext();
+
+        if (IsADeleteBlock(*context, *ifStmt->getThen(), pointer) == false) {
+            return true;
+        }
+        addViolation(ifStmt, this);
+        return true;
+    }
+};
+
+oclint::RuleSet DeleteNullptrIsSafeRule::rules(new DeleteNullptrIsSafeRule);
---
+++ oclint-rules/test/redundant/DeleteNullptrIsSafeRuleTest.cpp
@@ -0,0 +1,62 @@
+#include "TestHeaders.h"
+
+#include "rules/redundant/DeleteNullptrIsSafeRule.cpp"
+
+TEST(DeleteNullptrIsSafeRuleTest, PropertyTest)
+{
+    DeleteNullptrIsSafeRule rule;
+    EXPECT_EQ(3, rule.priority());
+    EXPECT_EQ("delete nullptr is safe", rule.name());
+}
+
+TEST(DeleteNullptrIsSafeRuleTest, IfDelete)
+{
+    testRuleOnCXXCode(new DeleteNullptrIsSafeRule(),
+    //            1         2         3
+    //   123456789012345678901234567890123456
+        "void m(char* c) { if (c) delete c; }",
+        0, 1, 19, 1, 33);
+}
+
+TEST(DeleteNullptrIsSafeRuleTest, Comparator)
+{
+    //                            1         2         3         4
+    //                   12345678901234567890123456789012345678901234567
+    const char* code1 = "void m(char* c) { if (c != 0)        delete c; }";
+    const char* code2 = "void m(char* c) { if (c != nullptr)  delete c; }";
+    const char* code3 = "void m(char* c) { if (c != (void*)0) delete c; }";
+
+    testRuleOnCXXCode(new DeleteNullptrIsSafeRule(), code1, 0, 1, 19, 1, 45);
+    testRuleOnCXX11Code(new DeleteNullptrIsSafeRule(), code2, 0, 1, 19, 1, 45);
+    testRuleOnCXXCode(new DeleteNullptrIsSafeRule(), code3, 0, 1, 19, 1, 45);
+}
+
+TEST(DeleteNullptrIsSafeRuleTest, ResetDeleteBlock)
+{
+    //                            1         2         3         4         5
+    //                   12345678901234567890123456789012345678901234567890
+    const char* code1 = "void m(char* c) { if (c) { delete c; c = 0;       } }";
+    const char* code2 = "void m(char* c) { if (c) { delete c; c = nullptr; } }";
+
+    testRuleOnCXXCode(new DeleteNullptrIsSafeRule(), code1, 0, 1, 19, 1, 51);
+    testRuleOnCXX11Code(new DeleteNullptrIsSafeRule(), code2, 0, 1, 19, 1, 51);
+}
+
+TEST(DeleteNullptrIsSafeRuleTest, CorrectCode)
+{
+    const char* code1 = "void m(char* c) { if (c) { delete c; } else ; }";
+    const char* code2 = "void m(char* c) { if (c) delete c; else ; }";
+    const char* code3 = "void m(char* c) { if (c) { delete c; c = 0; ++c; } }";
+    const char* code4 = "void m(char* c, char* b) { if (c) { delete c; c = b; } }";
+
+    testRuleOnCXXCode(new DeleteNullptrIsSafeRule(), code1);
+    testRuleOnCXXCode(new DeleteNullptrIsSafeRule(), code2);
+    testRuleOnCXXCode(new DeleteNullptrIsSafeRule(), code3);
+    testRuleOnCXXCode(new DeleteNullptrIsSafeRule(), code4);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
