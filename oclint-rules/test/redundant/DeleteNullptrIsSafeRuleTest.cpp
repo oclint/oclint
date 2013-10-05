342394cc85e581caf994ac81df3ab637f0f01919
Jarod42 <joris.dauphin@gmail.com>
2013-10-05 11:24:33 +0200

    Add unitTest with `delete []`.

--- oclint-rules/test/redundant/DeleteNullptrIsSafeRuleTest.cpp
+++ oclint-rules/test/redundant/DeleteNullptrIsSafeRuleTest.cpp
@@ -42,6 +42,17 @@ TEST(DeleteNullptrIsSafeRuleTest, ResetDeleteBlock)
     testRuleOnCXX11Code(new DeleteNullptrIsSafeRule(), code2, 0, 1, 19, 1, 51);
 }
 
+TEST(DeleteNullptrIsSafeRuleTest, DeleteArray)
+{
+    //                            1         2         3         4
+    //                   12345678901234567890123456789012345678901234567
+    const char* code1 = "void m(char* c) { if (c != 0) delete [] c; }";
+    const char* code2 = "struct S {~S();}; void m(S* s) {if (s) delete[]s;}";
+
+    testRuleOnCXXCode(new DeleteNullptrIsSafeRule(), code1, 0, 1, 19, 1, 41);
+    testRuleOnCXXCode(new DeleteNullptrIsSafeRule(), code2, 0, 1, 33, 1, 48);
+}
+
 TEST(DeleteNullptrIsSafeRuleTest, CorrectCode)
 {
     const char* code1 = "void m(char* c) { if (c) { delete c; } else ; }";
