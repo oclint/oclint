751b1b6d0203db76ca1c96da2cd50b7324959fbb
Akiva Leffert <akiva@etsy.com>
2013-10-31 14:22:29 -0400

    Add back moved files. Improve test story

---
+++ oclint-rules/rules/cocoa/ObjCVerifyMustCallSuperRule.cpp
@@ -0,0 +1,164 @@
+#include <clang/AST/Attr.h>
+
+#include "oclint/AbstractASTVisitorRule.h"
+#include "oclint/RuleSet.h"
+
+using namespace std;
+using namespace clang;
+using namespace oclint;
+
+class ContainsCallToSuperMethod : public RecursiveASTVisitor<ContainsCallToSuperMethod>
+{
+private:
+    string _selector;
+
+    // Location to save found ivar accesses
+    bool _foundSuperCall;
+public:
+
+    explicit ContainsCallToSuperMethod (string s) : _selector(s)  {
+        _foundSuperCall = false;
+    }
+
+    bool VisitObjCMessageExpr(ObjCMessageExpr* expr)
+    {
+        if(expr->getSelector().getAsString() == _selector
+        && expr->getReceiverKind() == ObjCMessageExpr::SuperInstance) {
+            _foundSuperCall = true;
+        }
+        return true;
+    }
+
+    bool foundSuperCall() const {
+        return _foundSuperCall;
+    }
+};
+
+
+class ObjCVerifyMustCallSuperRule : public AbstractASTVisitorRule<ObjCVerifyMustCallSuperRule>
+{
+private:
+    static RuleSet rules;
+
+    map<string, vector<string>> _libraryCases;
+
+    bool markedAsNeedsSuper(const ObjCMethodDecl *decl)
+    {
+        // Check if we have an attribute
+        for (auto attr = decl->specific_attr_begin<AnnotateAttr>(),
+            attrEnd = decl->specific_attr_end<AnnotateAttr>();
+            attr != attrEnd;
+            ++attr)
+        {
+            AnnotateAttr *annotate = dyn_cast<AnnotateAttr>(*attr);
+
+            // TODO add a mechanism to separate the annotation name from the rule name and use that
+            if (annotate && (annotate->getAnnotation() == "oclint:enforce[must call super]"))
+            {
+                return true;
+            }
+        }
+
+
+        return false;
+    }
+
+    bool declHasParentClassNamed(const ObjCMethodDecl* decl, string className) {
+        const ObjCInterfaceDecl* interface = decl->getClassInterface();
+        while(interface->getSuperClass() != NULL) {
+            interface = interface->getSuperClass();
+            if(interface->getNameAsString() == className) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool isLibraryCase(const ObjCMethodDecl* decl) {
+        string selectorName = decl->getSelector().getAsString();
+        auto classNames = _libraryCases.find(selectorName);
+        if(classNames != _libraryCases.end()) {
+            auto classes = classNames->second;
+            for(auto it = classes.begin(), ite = classes.end(); it != ite; ++it) {
+                if(declHasParentClassNamed(decl, *it)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    bool declRequiresSuperCall(ObjCMethodDecl* decl) {
+        if(isLibraryCase(decl)) {
+            return true;
+        }
+
+        if(decl->isOverriding()) {
+            SmallVector<const ObjCMethodDecl*, 4> overridden;
+            decl->getOverriddenMethods(overridden);
+            for(auto it=overridden.begin(), ite = overridden.end(); it != ite; ++it) {
+                if(markedAsNeedsSuper(*it)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+
+public:
+    
+    ObjCVerifyMustCallSuperRule() {
+        // UIKit cases
+        _libraryCases["viewWillAppear:"] = {"UIViewController"};
+        _libraryCases["viewDidAppear:"] = {"UIViewController"};
+        _libraryCases["viewWillDisappear:"] = {"UIViewController"};
+        _libraryCases["viewDidDisappear:"] = {"UIViewController"};
+        _libraryCases["viewDidLayoutSubviews"] = {"UIViewController"};
+        _libraryCases["layoutSubviews"] = {"UIView"};
+        _libraryCases["updateConstraints"] = {"UIView"};
+        _libraryCases["viewDidLoad"] = {"UIView"};
+        _libraryCases["reset"] = {"UIGestureRecognizer"};
+        _libraryCases["canPreventGestureRecognizer:"] = {"UIGestureRecognizer"};
+        _libraryCases["canBePreventedByGestureRecognizer:"] = {"UIGestureRecognizer"};
+        _libraryCases["shouldRequireFailureOfGestureRecognizer:"] = {"UIGestureRecognizer"};
+        _libraryCases["shouldBeRequiredToFailByGestureRecognizer:"] = {"UIGestureRecognizer"};
+        _libraryCases["touchesBegan:withEvent:"] = {"UIGestureRecognizer"};
+        _libraryCases["touchesMoved:withEvent:"] = {"UIGestureRecognizer"};
+        _libraryCases["touchesEnded:withEvent:"] = {"UIViewController"};
+        _libraryCases["touchesCancelled:withEvent:"] = {"UIGestureRecognizer"};
+    }
+
+    virtual const string name() const
+    {
+        return "must call super";
+    }
+
+    virtual int priority() const
+    {
+        return 1;
+    }
+    bool VisitObjCMethodDecl(ObjCMethodDecl* decl) {
+        // Save the method name
+        string selectorName = decl->getSelector().getAsString();
+
+        // Figure out if anything in the super chain is marked
+        if(declRequiresSuperCall(decl)) {
+            // If so, start a separate checker to look for method sends just in the method body
+            ContainsCallToSuperMethod checker(selectorName);
+            checker.TraverseDecl(decl);
+            if(!checker.foundSuperCall()) {
+                string message = "overridden method " + selectorName + " must call super";
+                addViolation(decl, this, message);
+            }
+        }
+
+        return true;
+    }
+
+};
+
+
+RuleSet ObjCVerifyMustCallSuperRule::rules(new ObjCVerifyMustCallSuperRule());
---
+++ oclint-rules/test/cocoa/ObjCVerifyMustCallSuperRuleTest.cpp
@@ -0,0 +1,160 @@
+#include "TestHeaders.h"
+#include "rules/cocoa/ObjCVerifyMustCallSuperRule.cpp"
+
+static string testDoesCall = "\
+                                                    \n\
+@implementation ChildViewController                 \n\
+                                                    \n\
+- (void)viewWillAppear:(BOOL)animated {             \n\
+    [super viewWillAppear:animated];                \n\
+}                                                   \n\
+                                                    \n\
+@end                                                \n\
+                                                    \n\
+";
+
+static string testDoesNotCall = "\
+                                                    \n\
+@implementation ChildViewController                 \n\
+                                                    \n\
+- (void)viewWillAppear:(BOOL)animated {             \n\
+}                                                   \n\
+                                                    \n\
+@end                                                \n\
+                                                    \n\
+";
+
+static string testLibraryBase = "\
+typedef unsigned char BOOL;                         \n\
+                                                    \n\
+@interface UIViewController                         \n\
+                                                    \n\
+- (void)viewWillAppear:(BOOL)animated;              \n\
+                                                    \n\
+@end                                                \n\
+                                                    \n\
+                                                    \n\
+@interface ChildViewController : UIViewController   \n\
+                                                    \n\
+@end                                                \n\
+";
+
+static string testLibraryDoesCall = testLibraryBase + testDoesCall;
+static string testLibraryDoesNotCall = testLibraryBase + testDoesNotCall;
+
+static string testAnnotationBase = "\
+typedef unsigned char BOOL;                                                                    \n\
+@interface NSObject                                                                            \n\
+@end                                                                                           \n\
+                                                                                               \n\
+@interface SomeBaseClass : NSObject                                                            \n\
+                                                                                               \n\
+- (void)viewWillAppear:(BOOL)animated                                                          \n\
+__attribute__((annotate(\"oclint:enforce[must call super]\")));                                \n\
+                                                                                               \n\
+@end                                                                                           \n\
+                                                                                               \n\
+                                                                                               \n\
+@interface ChildViewController : SomeBaseClass                                                 \n\
+                                                                                               \n\
+@end                                                                                           \n\
+";
+
+static string testAnnotationDoesCall = testAnnotationBase + testDoesCall;
+static string testAnnotationDoesNotCall = testAnnotationBase + testDoesNotCall;
+
+static string testSuppression = "\
+@implementation ChildViewController                                             \n\
+                                                                                \n\
+- (void)viewWillAppear:(BOOL)animated                                           \n\
+    __attribute__((annotate(\"oclint:suppress[must call super]\"))) {           \n\
+}                                                                               \n\
+                                                                                \n\
+@end                                                                            \n\
+                                                                                \n\
+";
+
+
+static string testAnnotationSuppression = testAnnotationBase + testSuppression;
+static string testLibrarySuppression = testLibraryBase + testSuppression;
+
+static string testNormalMethod = "\
+@interface NSObject                                 \n\
+@end                                                \n\
+                                                    \n\
+@interface ControllerBase : NSObject                \n\
+                                                    \n\
+- (void)someMethod;                                 \n\
+                                                    \n\
+@end                                                \n\
+                                                    \n\
+@implementation ControllerBase                      \n\
+                                                    \n\
+- (void)someMethod {                                \n\
+}                                                   \n\
+                                                    \n\
+@end                                                \n\
+                                                    \n\
+@interface ChildViewController : ControllerBase     \n\
+                                                    \n\
+@end                                                \n\
+                                                    \n\
+@implementation ChildViewController                 \n\
+                                                    \n\
+- (void)anotherMethod {                             \n\
+}                                                   \n\
+@end                                                \n\
+                                                    \n\
+";
+
+TEST(ObjcVerifyMustCallSuperRuleTest, PropertyTest)
+{
+    ObjCVerifyMustCallSuperRule rule;
+    EXPECT_EQ(1, rule.priority());
+    EXPECT_EQ("must call super", rule.name());
+}
+
+TEST(ObjcVerifyMustCallSuperRuleTest, LibraryDoesCall)
+{
+    testRuleOnObjCCode(new ObjCVerifyMustCallSuperRule(), testLibraryDoesCall);
+}
+
+TEST(ObjcVerifyMustCallSuperRuleTest, LibraryDoesNotCall)
+{
+    testRuleOnObjCCode(new ObjCVerifyMustCallSuperRule(),
+        testLibraryDoesNotCall, 0, 16, 1, 17, 1,
+        "overridden method viewWillAppear: must call super");
+}
+
+TEST(ObjcVerifyMustCallSuperRuleTest, AnnotationDoesCall)
+{
+    testRuleOnObjCCode(new ObjCVerifyMustCallSuperRule(), testAnnotationDoesCall);
+}
+
+TEST(ObjcVerifyMustCallSuperRuleTest, AnnotationDoesNotCall)
+{
+    testRuleOnObjCCode(new ObjCVerifyMustCallSuperRule(),
+        testAnnotationDoesNotCall, 0, 19, 1, 20, 1,
+        "overridden method viewWillAppear: must call super");
+}
+
+TEST(ObjcVerifyMustCallSuperRuleTest, LibrarySuppression)
+{
+    testRuleOnObjCCode(new ObjCVerifyMustCallSuperRule(), testLibrarySuppression);
+}
+
+TEST(ObjcVerifyMustCallSuperRuleTest, AnnotationSuppression)
+{
+    testRuleOnObjCCode(new ObjCVerifyMustCallSuperRule(), testAnnotationSuppression);
+}
+
+TEST(ObjcVerifyMustCallSuperRuleTest, NormalMethod)
+{
+    testRuleOnObjCCode(new ObjCVerifyMustCallSuperRule(), testNormalMethod);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
