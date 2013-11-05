3f3689d164fe0ab88b68fcf3dd345f9ae9d6efb6
Jarod42 <joris.dauphin@gmail.com>
2013-11-05 11:58:52 +0100

    Use Tag location in redundant/*RuleTest.cpp

--- oclint-rules/test/redundant/UnnecessaryElseStatementRuleTest.cpp
+++ oclint-rules/test/redundant/UnnecessaryElseStatementRuleTest.cpp
@@ -21,55 +21,66 @@ TEST(UnnecessaryElseStatementRuleTest, IfStmtHasNoReturn)
 
 TEST(UnnecessaryElseStatementRuleTest, IfThenStmtIsReturn)
 {
-    testRuleOnCode(new UnnecessaryElseStatementRule(), "void m() { if(1) return; else {} }",
-        0, 1, 31, 1, 32);
+    testRuleOnCode(new UnnecessaryElseStatementRule(),
+        "void m() { if(1) return; else " VIOLATION_START "{" VIOLATION_END "} }");
 }
 
 TEST(UnnecessaryElseStatementRuleTest, IfThenBlockHasReturn)
 {
-    testRuleOnCode(new UnnecessaryElseStatementRule(), "void m() { if(1) { return; } else {} }",
-        0, 1, 35, 1, 36);
+    testRuleOnCode(new UnnecessaryElseStatementRule(),
+        "void m() { if(1) { return; } else " VIOLATION_START "{" VIOLATION_END "} }");
 }
 
 TEST(UnnecessaryElseStatementRuleTest, NotEveryBranchEndWithReturn)
 {
-    testRuleOnCode(new UnnecessaryElseStatementRule(), "void m() { if(1) { if(0) { if(1) return; } } else {} }");
+    testRuleOnCode(new UnnecessaryElseStatementRule(),
+        "void m() { if(1) { if(0) { if(1) return; } } else {} }");
 }
 
 TEST(UnnecessaryElseStatementRuleTest, ElseIfShouldBeIgnored)
 {
-    testRuleOnCode(new UnnecessaryElseStatementRule(), "void m() { if(1) { return; } else if(0) { return; } }");
+    testRuleOnCode(new UnnecessaryElseStatementRule(),
+        "void m() { if(1) { return; } else if(0) { return; } }");
 }
 
 TEST(UnnecessaryElseStatementRuleTest, NestedElseIf)
 {
-    testRuleOnCode(new UnnecessaryElseStatementRule(), "void m() { if(1) { return; } else if (0) { return; } else {} }",
-        0, 1, 59, 1, 60);
+    testRuleOnCode(new UnnecessaryElseStatementRule(),
+        "void m() { if(1) { return; } else if (0) { return; } else " VIOLATION_START "{" VIOLATION_END "} }");
 }
 
 TEST(UnnecessaryElseStatementRuleTest, NestedElseIfNotAllBranchesEndWithReturn)
 {
-    testRuleOnCode(new UnnecessaryElseStatementRule(), "void m() { int i; if(1) { i = 1; } else if (0) {return;} else { i = 0; } }");
+    testRuleOnCode(new UnnecessaryElseStatementRule(),
+        "void m() { int i; if(1) { i = 1; } else if (0) {return;} else { i = 0; } }");
 }
 
 TEST(UnnecessaryElseStatementRuleTest, IfThenBlockContainsReturnAsLast)
 {
-    testRuleOnCode(new UnnecessaryElseStatementRule(), "void m() { if(1) { int i = 1; return; } else {} }",
-        0, 1, 46, 1, 47);
+    testRuleOnCode(new UnnecessaryElseStatementRule(),
+        "void m() { if(1) { int i = 1; return; } else " VIOLATION_START "{" VIOLATION_END "} }");
 }
 
 TEST(UnnecessaryElseStatementRuleTest, IfThenBlockContainsReturnNotLast)
 {
-    testRuleOnCode(new UnnecessaryElseStatementRule(), "void m() { if(1) { return; int i = 2; } else {} }",
-        0, 1, 46, 1, 47);
+    testRuleOnCode(new UnnecessaryElseStatementRule(),
+        "void m() { if(1) { return; int i = 2; } else " VIOLATION_START "{" VIOLATION_END "} }");
 }
 
 TEST(UnnecessaryElseStatementRuleTest, EmbeddedIfStatementsEndingWithReturn)
 {
+#if 0
+    // expect increasing order...
+    // but rule provide violation in descending order ?
+    testRuleOnCode(new UnnecessaryElseStatementRule(),
+        "void m() { if (1) { if (0) { return; } else " VIOLATION_START "{ return;" VIOLATION_END
+        " } } else " VIOLATION_START "{ int i = 0; " VIOLATION_END "} }");
+#else
     testRuleOnCode(new UnnecessaryElseStatementRule(), "void m() { if (1) { if (0) { return; } else { return; } } else { int i = 0; } }",
         0, 1, 64, 1, 77);
     testRuleOnCode(new UnnecessaryElseStatementRule(), "void m() { if (1) { if (0) { return; } else { return; } } else { int i = 0; } }",
         1, 1, 45, 1, 55);
+#endif
 }
 
 int main(int argc, char **argv)
