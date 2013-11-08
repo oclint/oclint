ea2ccff8b13ce9d924dfb886b3ca8788b5ba891e
Akiva Leffert <akiva@etsy.com>
2013-11-08 17:16:35 -0500

    Check for protected methods

---
+++ oclint-rules/rules/cocoa/ObjCVerifyProtectedMethodRule.cpp
@@ -0,0 +1,88 @@
+#include <clang/AST/Attr.h>
+
+#include "oclint/AbstractASTVisitorRule.h"
+#include "oclint/RuleSet.h"
+#include "oclint/helper/EnforceHelper.h"
+#include "oclint/util/ASTUtil.h"
+
+using namespace std;
+using namespace clang;
+using namespace oclint;
+
+namespace {
+
+    class CheckMethodsInsideClass : public RecursiveASTVisitor<CheckMethodsInsideClass>
+    {
+        private:
+            ObjCInterfaceDecl& _container;
+            AbstractASTRuleBase& _rule;
+            vector<Expr*> _violations;
+
+        public:
+            CheckMethodsInsideClass(ObjCInterfaceDecl& container, AbstractASTRuleBase& rule) :
+                _container(container), _rule(rule) {};
+
+        bool VisitObjCMessageExpr(ObjCMessageExpr* expr) {
+            const auto method = expr->getMethodDecl();
+            const bool isMarked = declHasEnforceAttribute(method, _rule);
+            if(!isMarked) {
+                return true;
+            }
+
+            const auto interface = expr->getReceiverInterface();
+            if(!interface) {
+                return true;
+            }
+
+            bool insideChild = false;
+            for(auto current = interface; current != nullptr; current = current->getSuperClass()) {
+                insideChild = insideChild || current->getDeclName() == _container.getDeclName();
+            }
+            if(!insideChild) {
+                _violations.push_back(expr);
+            }
+
+            return true;
+        }
+
+        vector <Expr*> getViolations() {
+            return _violations;
+        }
+
+    };
+}
+
+class ObjCVerifyProtectedMethodRule : public AbstractASTVisitorRule<ObjCVerifyProtectedMethodRule>
+{
+private:
+    static RuleSet rules;
+
+public:
+    bool VisitObjCImplementationDecl(ObjCImplementationDecl* decl) {
+        const auto interface = decl->getClassInterface();
+
+        if(interface) {
+            auto checker = CheckMethodsInsideClass(*interface, *this);
+            checker.TraverseDecl(decl);
+            auto violations = checker.getViolations();
+            for(auto it = violations.begin(), end = violations.end(); it != end; ++it) {
+                addViolation(*it, this);
+            }
+        }
+        return true;
+    }
+
+    virtual const string name() const
+    {
+        return "protected method";
+    }
+
+    virtual int priority() const
+    {
+        return 1;
+    }
+
+};
+
+
+RuleSet ObjCVerifyProtectedMethodRule::rules(new ObjCVerifyProtectedMethodRule());
---
+++ oclint-rules/test/cocoa/ObjCVerifyProtectedMethodRuleTest.cpp
@@ -0,0 +1,87 @@
+#include "TestHeaders.h"
+#include "rules/cocoa/ObjCVerifyProtectedMethodRule.cpp"
+
+string testAnnotationBase = "\
+@interface NSObject                                                          \n\
+                                                                             \n\
+@end                                                                         \n\
+                                                                             \n\
+@interface A : NSObject                                                      \n\
+                                                                             \n\
+- (void)foo __attribute__((annotate(\"oclint:enforce[protected method]\"))); \n\
+                                                                             \n\
+@end                                                                         \n\
+                                                                             \n\
+@interface B : NSObject                                                      \n\
+                                                                             \n\
+@property (strong, nonatomic) A* a;                                          \n\
+                                                                             \n\
+@end                                                                         \n\
+";
+
+string testViolation =  testAnnotationBase + "\
+@implementation B                                                            \n\
+                                                                             \n\
+- (void)bar {                                                                \n\
+    [self.a foo];                                                            \n\
+}                                                                            \n\
+                                                                             \n\
+@end                                                                         \n\
+";
+
+string testInsideClass = testAnnotationBase + "\
+@implementation A                                                            \n\
+                                                                             \n\
+- (void)foo {                                                                \n\
+}                                                                            \n\
+                                                                             \n\
+- (void)bar {                                                                \n\
+    [self foo];                                                              \n\
+}                                                                            \n\
+                                                                             \n\
+@end                                                                         \n\
+";
+
+string testInsideChild = testAnnotationBase + "\
+@interface C : A                                                             \n\
+                                                                             \n\
+- (void)bar;                                                                 \n\
+                                                                             \n\
+@end                                                                         \n\
+                                                                             \n\
+@implementation C                                                            \n\
+                                                                             \n\
+- (void)bar {                                                                \n\
+    [self foo];                                                              \n\
+}                                                                            \n\
+                                                                             \n\
+@end                                                                         \n\
+";
+
+TEST(ObjCVerifyProtectedMethodRule, PropertyTest)
+{
+    ObjCVerifyProtectedMethodRule rule;
+    EXPECT_EQ(1, rule.priority());
+    EXPECT_EQ("protected method", rule.name());
+}
+
+TEST(ObjCVerifyProtectedMethodRule, ViolationTest)
+{
+    testRuleOnObjCCode(new ObjCVerifyProtectedMethodRule(), testViolation, 0, 19, 5, 19, 16);
+}
+
+TEST(ObjCVerifyProtectedMethodRule, InsideClassTest)
+{
+    testRuleOnObjCCode(new ObjCVerifyProtectedMethodRule(), testInsideClass);
+}
+
+TEST(ObjCVerifyProtectedMethodRule, InsideChildTest)
+{
+    testRuleOnObjCCode(new ObjCVerifyProtectedMethodRule(), testInsideChild);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
