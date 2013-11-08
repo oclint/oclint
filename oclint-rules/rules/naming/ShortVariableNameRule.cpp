8f8f70eeba1d63be5db83756bb0cdcbe6fc6a0bd
Longyi Qi <lqi+git@longyiqi.com>
2013-11-08 15:37:36 -0500

    ForEach loop and multiple init var decl are suppressed for short variable name rule

--- oclint-rules/rules/naming/ShortVariableNameRule.cpp
+++ oclint-rules/rules/naming/ShortVariableNameRule.cpp
@@ -1,3 +1,5 @@
+#include <stack>
+
 #include "oclint/AbstractASTVisitorRule.h"
 #include "oclint/RuleConfiguration.h"
 #include "oclint/RuleSet.h"
@@ -11,7 +13,44 @@ class ShortVariableNameRule : public AbstractASTVisitorRule<ShortVariableNameRul
 {
 private:
     static RuleSet rules;
-    VarDecl *_suppressVarDecl = NULL;
+    std::stack<VarDecl *> _suppressVarDecls;
+
+    void clearVarDeclsStack()
+    {
+        while (!_suppressVarDecls.empty())
+        {
+            _suppressVarDecls.pop();
+        }
+    }
+
+    void handleVarDecl(VarDecl *varDecl)
+    {
+        if (varDecl)
+        {
+            _suppressVarDecls.push(varDecl);
+        }
+    }
+
+    void handleDeclStmt(Stmt *initStmt)
+    {
+        DeclStmt *declStmt = dyn_cast_or_null<DeclStmt>(initStmt);
+        if (!declStmt)
+        {
+            return;
+        }
+        if (declStmt->isSingleDecl())
+        {
+            handleVarDecl(dyn_cast_or_null<VarDecl>(declStmt->getSingleDecl()));
+        }
+        else
+        {
+            for (DeclStmt::reverse_decl_iterator it = declStmt->decl_rbegin(),
+                itEnd = declStmt->decl_rend(); it != itEnd; it++)
+            {
+                handleVarDecl(dyn_cast_or_null<VarDecl>(*it));
+            }
+        }
+    }
 
 public:
     virtual const string name() const
@@ -28,32 +67,60 @@ public:
     {
         int nameLength = varDecl->getNameAsString().size();
         int threshold = RuleConfiguration::intForKey("SHORT_VARIABLE_NAME", 3);
-        if (nameLength > 0 && nameLength < threshold && varDecl != _suppressVarDecl)
+        if (nameLength <= 0 || nameLength >= threshold)
         {
-            string description = "Variable name with " + toString<int>(nameLength) +
-                " characters is shorter than the threshold of " + toString<int>(threshold);
-            addViolation(varDecl, this, description);
+            return true;
         }
 
-        _suppressVarDecl = NULL;
+        if (!_suppressVarDecls.empty())
+        {
+            VarDecl *topVarDecl = _suppressVarDecls.top();
+            _suppressVarDecls.pop();
+            if (varDecl == topVarDecl)
+            {
+                return true;
+            }
+        }
+
+        string description = "Variable name with " + toString<int>(nameLength) +
+            " characters is shorter than the threshold of " + toString<int>(threshold);
+        addViolation(varDecl, this, description);
 
         return true;
     }
 
     bool VisitForStmt(ForStmt *forStmt)
     {
-        DeclStmt *declStmt = dyn_cast_or_null<DeclStmt>(forStmt->getInit());
-        if (declStmt && declStmt->isSingleDecl())
-        {
-            _suppressVarDecl = dyn_cast<VarDecl>(declStmt->getSingleDecl());
-        }
+        clearVarDeclsStack();
+        handleDeclStmt(forStmt->getInit());
 
         return true;
     }
 
+    bool VisitCXXForRangeStmt(CXXForRangeStmt *forStmt)
+    {
+        clearVarDeclsStack();
+        handleVarDecl(forStmt->getLoopVariable());
+
+        return true;
+    }
+
+
+    bool VisitObjCForCollectionStmt(ObjCForCollectionStmt *forStmt)
+    {
+        clearVarDeclsStack();
+        handleDeclStmt(forStmt->getElement());
+
+        return true;
+    }
+
+
+
     bool VisitCXXCatchStmt(CXXCatchStmt *catchStmt)
     {
-        _suppressVarDecl = catchStmt->getExceptionDecl();
+        clearVarDeclsStack();
+        handleVarDecl(catchStmt->getExceptionDecl());
+
         return true;
     }
 };
