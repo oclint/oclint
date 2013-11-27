f5b7733c691f3140f1fa7eb7eb4235d57724c861
Longyi Qi <lqi+git@longyiqi.com>
2013-11-27 16:57:03 -0600

    Changes for unix-like platform; small quality improvement

--- oclint-rules/test/headers/TestRuleOnCode.h
+++ oclint-rules/test/headers/TestRuleOnCode.h
@@ -1,11 +1,11 @@
 inline bool computeViolationSet(const Twine &fileName,
-                                RuleBase *rule,
-                                const std::string &code,
-                                const std::vector<std::string> &args,
-                                ViolationSet *violationSet)
+    RuleBase *rule,
+    const std::string &code,
+    const std::vector<std::string> &args,
+    ViolationSet *violationSet)
 {
-    TestProcessor processor(rule, violationSet);
-    FrontendAction* action = new TestFrontendAction(&processor);
+    TestProcessor *processor = new TestProcessor(rule, violationSet);
+    FrontendAction* action = new TestFrontendAction(processor);
     Twine twine(code);
 
     const std::size_t randomPrefixLength = 6;
@@ -18,29 +18,30 @@ inline bool computeViolationSet(const Twine &fileName,
     return runToolOnCodeWithArgs(action, twine, args, randomPrefix + fileName);
 }
 
-#if 1 // Deprecated functions
+inline void validateViolation(const Violation& violation,
+    int expectStartLine,
+    int expectStartColumn,
+    int expectEndLine,
+    int expectEndColumn,
+    const string& expectMessage = "")
+{
+    EXPECT_THAT(violation.startLine, Eq(expectStartLine));
+    EXPECT_THAT(violation.startColumn, Eq(expectStartColumn));
+    EXPECT_THAT(violation.endLine, Eq(expectEndLine));
+    EXPECT_THAT(violation.endColumn, Eq(expectEndColumn));
+    EXPECT_THAT(violation.message, StrEq(expectMessage));
+}
 
 inline void testRuleOnCode(const Twine &fileName,
     RuleBase *rule,
     const string &code,
     const std::vector<std::string> &args,
     int violationIndex,
-    int expectStartLine,
-    int expectStartColumn,
-    int expectEndLine,
-    int expectEndColumn,
-    const string& expectMessage = "") /*__attribute__ ((deprecated))*/;
-
-inline void testRuleOnCode(const Twine &fileName,
-    RuleBase *rule,
-    const string &code,
-    const std::vector<std::string> &args,
-    int violationIndex,
-    int expectStartLine,
-    int expectStartColumn,
-    int expectEndLine,
-    int expectEndColumn,
-    const string& expectMessage)
+    int startLine,
+    int startColumn,
+    int endLine,
+    int endColumn,
+    const string& message = "")
 {
     ViolationSet violationSet;
 
@@ -58,11 +59,7 @@ inline void testRuleOnCode(const Twine &fileName,
             if (size_t(violationIndex) < violations.size())
             {
                 const Violation& violation = violations.at(violationIndex);
-                EXPECT_THAT(violation.startLine, Eq(expectStartLine));
-                EXPECT_THAT(violation.startColumn, Eq(expectStartColumn));
-                EXPECT_THAT(violation.endLine, Eq(expectEndLine));
-                EXPECT_THAT(violation.endColumn, Eq(expectEndColumn));
-                EXPECT_THAT(violation.message, StrEq(expectMessage));
+                validateViolation(violation, startLine, startColumn, endLine, endColumn, message);
             }
         }
     }
@@ -79,16 +76,7 @@ inline void testRuleOnCode(RuleBase *rule,
     int expectStartColumn,
     int expectEndLine,
     int expectEndColumn,
-    const string& expectMessage = "") __attribute__ ((deprecated));
-
-inline void testRuleOnCode(RuleBase *rule,
-    const string &code,
-    int violationIndex,
-    int expectStartLine,
-    int expectStartColumn,
-    int expectEndLine,
-    int expectEndColumn,
-    const string& expectMessage)
+    const string& expectMessage = "")
 {
     testRuleOnCode("input.c", rule, code, {}, violationIndex,
         expectStartLine, expectStartColumn, expectEndLine, expectEndColumn, expectMessage);
@@ -101,16 +89,7 @@ inline void testRuleOnCXXCode(RuleBase *rule,
     int expectStartColumn,
     int expectEndLine,
     int expectEndColumn,
-    const string& expectMessage = "") __attribute__ ((deprecated));
-
-inline void testRuleOnCXXCode(RuleBase *rule,
-    const string &code,
-    int violationIndex,
-    int expectStartLine,
-    int expectStartColumn,
-    int expectEndLine,
-    int expectEndColumn,
-    const string& expectMessage)
+    const string& expectMessage = "")
 {
     testRuleOnCode("input.cpp", rule, code, {}, violationIndex,
         expectStartLine, expectStartColumn, expectEndLine, expectEndColumn, expectMessage);
@@ -123,16 +102,7 @@ inline void testRuleOnCXX11Code(RuleBase *rule,
     int expectStartColumn,
     int expectEndLine,
     int expectEndColumn,
-    const string& expectMessage = "") __attribute__ ((deprecated));
-
-inline void testRuleOnCXX11Code(RuleBase *rule,
-    const string &code,
-    int violationIndex,
-    int expectStartLine,
-    int expectStartColumn,
-    int expectEndLine,
-    int expectEndColumn,
-    const string& expectMessage)
+    const string& expectMessage = "")
 {
     testRuleOnCode("input.cpp", rule, code, {"-std=c++11"}, violationIndex,
         expectStartLine, expectStartColumn, expectEndLine, expectEndColumn, expectMessage);
@@ -145,22 +115,11 @@ inline void testRuleOnObjCCode(RuleBase *rule,
     int expectStartColumn,
     int expectEndLine,
     int expectEndColumn,
-    const string& expectMessage = "") __attribute__ ((deprecated));
-
-inline void testRuleOnObjCCode(RuleBase *rule,
-    const string &code,
-    int violationIndex,
-    int expectStartLine,
-    int expectStartColumn,
-    int expectEndLine,
-    int expectEndColumn,
-    const string& expectMessage)
+    const string& expectMessage = "")
 {
     testRuleOnCode("input.m", rule, code, {}, violationIndex,
         expectStartLine, expectStartColumn, expectEndLine, expectEndColumn, expectMessage);
 }
-
-#endif // Deprecated functions
 
 #define VIOLATION_START "/*VIOLATION_START*/"
 #define VIOLATION_END "/*VIOLATION_END*/"
@@ -173,22 +132,25 @@ inline void testRuleOnObjCCode(RuleBase *rule,
 #define V_START VIOLATION_START
 #define V_END VIOLATION_END
 
-inline void ComputeLocalisation(const std::string& s, size_t offset,
-                                int* line, int* column)
+inline void ComputeLocalisation(const std::string& code, size_t offset, int* line, int* column)
 {
-    int c = 1;
-    int l = 1;
+    int currentColumn = 1;
+    int currentLine = 1;
 
-    for (size_t i = 0; i != offset; ++i) {
-        if (s[i] == '\n') {
-            c = 1;
-            ++l;
-        } else {
-            ++c;
+    for (size_t i = 0; i != offset; ++i)
+    {
+        if (code[i] == '\n')
+        {
+            currentColumn = 1;
+            ++currentLine;
+        }
+        else
+        {
+            ++currentColumn;
         }
     }
-    *line = l;
-    *column = c;
+    *line = currentLine;
+    *column = currentColumn;
 }
 
 // {start/end offset} ordered by increasing start-location then end-location.
@@ -208,11 +170,14 @@ inline std::vector<std::pair<size_t, size_t>> extractViolationLocations(std::str
             std::sort(rangeOffsets.begin(), rangeOffsets.end());
             return rangeOffsets;
         }
-        if (startOffset < endOffset) {
+        if (startOffset < endOffset)
+        {
             code.erase(startOffset, strlen(VIOLATION_START));
             startOffsets.push_back(startOffset);
             findOffset = startOffset;
-        } else {
+        }
+        else
+        {
             code.erase(endOffset, strlen(VIOLATION_END));
             assert(!startOffsets.empty());
             rangeOffsets.push_back({startOffsets.back(), endOffset});
@@ -232,10 +197,10 @@ bool lessByLocation(const Violation& lhs, const Violation& rhs)
 }
 
 inline void testRuleOnCode(const Twine& filename,
-                           const std::vector<std::string>& args,
-                           RuleBase* rule,
-                           std::string code,
-                           const std::vector<std::string>& messages)
+    const std::vector<std::string>& args,
+    RuleBase* rule,
+    std::string code,
+    const std::vector<std::string>& messages)
 {
     const auto& rangeOffsets = extractViolationLocations(code);
     ViolationSet violationSet;
@@ -255,19 +220,11 @@ inline void testRuleOnCode(const Twine& filename,
         const Violation& violation = violations[i];
         const size_t startOffset = rangeOffsets[i].first;
         const size_t endOffset = rangeOffsets[i].second;
-        const std::string& expectedMessage = i < messages.size() ? messages[i] : "";
-        int expectedStartLine;
-        int expectedStartColumn;
-        int expectedEndLine;
-        int expectedEndColumn;
-
-        ComputeLocalisation(code, startOffset, &expectedStartLine, &expectedStartColumn);
-        ComputeLocalisation(code, endOffset, &expectedEndLine, &expectedEndColumn);
-        EXPECT_THAT(violation.startLine, Eq(expectedStartLine));
-        EXPECT_THAT(violation.startColumn, Eq(expectedStartColumn));
-        EXPECT_THAT(violation.endLine, Eq(expectedEndLine));
-        EXPECT_THAT(violation.endColumn, Eq(expectedEndColumn));
-        EXPECT_THAT(violation.message, StrEq(expectedMessage));
+        const std::string& message = i < messages.size() ? messages[i] : "";
+        int startLine, startColumn, endLine, endColumn;
+        ComputeLocalisation(code, startOffset, &startLine, &startColumn);
+        ComputeLocalisation(code, endOffset, &endLine, &endColumn);
+        validateViolation(violation, startLine, startColumn, endLine, endColumn, message);
     }
 }
 
