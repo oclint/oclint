be961f4ef3c705d87982817c787806131ab184b5
Longyi Qi <lqi+git@longyiqi.com>
2013-12-14 14:43:22 -0600

    Added branches and tags for llvm 3.4 release

--- oclint-scripts/oclintscripts/version.py
+++ oclint-scripts/oclintscripts/version.py
@@ -17,4 +17,4 @@ def oclint_dev_version():
     return oclint_version() + '.dev.' + git_hash()
 
 def llvm_branches():
-    return ['trunk', 'branches/release_33', 'tags/RELEASE_33/final', 'branches/release_32', 'tags/RELEASE_32/final']
+    return ['trunk', 'branches/release_34', 'tags/RELEASE_34/final', 'branches/release_33', 'tags/RELEASE_33/final', 'branches/release_32', 'tags/RELEASE_32/final']
