702ce544cc1f5c406b9838bb6a89258d8c76c261
Christian Taedcke <hacking@taedcke.com>
2014-03-09 11:55:30 +0100

    Add simple test for ViolationSet::operator==.

--- oclint-core/test/ViolationSetTest.cpp
+++ oclint-core/test/ViolationSetTest.cpp
@@ -21,9 +21,17 @@ protected:
     virtual void setUp()
     {
         ViolationSet ViolationSetTest_violationSet;
+        rule = new MockRuleBase();
+    }
+
+    virtual void tearDown()
+    {
+        delete rule;
     }
 
     ViolationSet ViolationSetTest_violationSet;
+
+    RuleBase* rule;
 };
 
 TEST_F(ViolationSetTest, EmptySet)
@@ -45,6 +53,38 @@ TEST_F(ViolationSetTest, AddViolation)
     EXPECT_THAT(compareViolation.startColumn, Eq(2));
     EXPECT_THAT(compareViolation.endLine, Eq(3));
     EXPECT_THAT(compareViolation.endColumn, Eq(4));
+}
+
+TEST_F(ViolationSetTest, EqualSameEntry)
+{
+    ViolationSet set1;
+    ViolationSet set2;
+    EXPECT_TRUE(set1 == set2);
+    EXPECT_TRUE(set2 == set1);
+    Violation violation1(rule, "", 1, 2, 3, 4);
+    set1.addViolation(violation1);
+    EXPECT_FALSE(set1 == set2);
+    EXPECT_FALSE(set2 == set1);
+    Violation violation2(rule, "", 1, 2, 3, 4);
+    set2.addViolation(violation2);
+    EXPECT_TRUE(set1 == set2);
+    EXPECT_TRUE(set2 == set1);
+}
+
+TEST_F(ViolationSetTest, EqualDifferentEntry)
+{
+    ViolationSet set1;
+    ViolationSet set2;
+    EXPECT_TRUE(set1 == set2);
+    EXPECT_TRUE(set2 == set1);
+    Violation violation1(rule, "", 1, 2, 3, 4);
+    set1.addViolation(violation1);
+    EXPECT_FALSE(set1 == set2);
+    EXPECT_FALSE(set2 == set1);
+    Violation violation2(rule, "", 1, 2, 3, 5);
+    set2.addViolation(violation2);
+    EXPECT_FALSE(set1 == set2);
+    EXPECT_FALSE(set2 == set1);
 }
 
 int main(int argc, char **argv)
