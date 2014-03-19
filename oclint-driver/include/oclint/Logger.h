61bdd6c3bb07e537b486a63678cb3ebb1234efeb
Longyi Qi <lqi+git@longyiqi.com>
2014-03-19 14:29:02 -0500

    Convert debug to llvm raw ostream

--- oclint-driver/include/oclint/Logger.h
+++ oclint-driver/include/oclint/Logger.h
@@ -1,33 +1,32 @@
 #ifndef OCLINT_LOGGER_H
 #define OCLINT_LOGGER_H
 
-#include <llvm/Support/Debug.h>
 #include <llvm/Support/raw_ostream.h>
 
 namespace oclint
 {
 namespace logger
 {
-    llvm::raw_ostream &verboseStream(bool debugFlag = false);
+#ifndef NDEBUG
+    llvm::raw_ostream &debugStream();
+#endif
+    llvm::raw_ostream &verboseStream();
 } // end namespace logger
 } // end namespace oclint
 
 #ifndef NDEBUG
 
-#define LOG_DEBUG(MSG) DEBUG(llvm::dbgs() << MSG)
+#define LOG_DEBUG(MSG) oclint::logger::debugStream() << MSG
 #define LOG_DEBUG_LINE(MSG) LOG_DEBUG(MSG << "\n")
-
-#define LOG_VERBOSE(MSG) oclint::logger::verboseStream(llvm::DebugFlag) << MSG
 
 #else
 
 #define LOG_DEBUG(MSG)
 #define LOG_DEBUG_LINE(MSG)
 
-#define LOG_VERBOSE(MSG) oclint::logger::verboseStream() << MSG
-
 #endif
 
+#define LOG_VERBOSE(MSG) oclint::logger::verboseStream() << MSG
 #define LOG_VERBOSE_LINE(MSG) LOG_VERBOSE(MSG << "\n")
 
 #endif
--- oclint-driver/lib/Logger.cpp
+++ oclint-driver/lib/Logger.cpp
@@ -1,10 +1,29 @@
 #include "oclint/Logger.h"
 
+#include <llvm/Support/Debug.h>
+
 #include "oclint/Options.h"
 
-llvm::raw_ostream &oclint::logger::verboseStream(bool debugFlag)
+#ifndef NDEBUG
+
+llvm::raw_ostream &oclint::logger::debugStream()
 {
-    if (debugFlag || oclint::option::enableVerbose())
+    if (llvm::DebugFlag)
+    {
+        return llvm::dbgs();
+    }
+    return llvm::nulls();
+}
+
+#endif
+
+llvm::raw_ostream &oclint::logger::verboseStream()
+{
+#ifndef NDEBUG
+    if (llvm::DebugFlag || oclint::option::enableVerbose())
+#else
+    if (oclint::option::enableVerbose())
+#endif
     {
         return llvm::outs();
     }
