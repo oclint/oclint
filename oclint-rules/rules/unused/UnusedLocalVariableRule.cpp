513532a178c8590779bfa5a3edc888c3029af29a
Martin Güthle <martin.guethle@bachmann-technology.com>
2015-10-06 00:33:31 +0200

    Disable the complain about the unused variales, if the they are used in RAII manner
    This protect the false positives about some basic classes such as std::lock_guard
    
    Some additional checks should be appended, to ensure that the given configuration is valid

--- oclint-rules/rules/unused/UnusedLocalVariableRule.cpp
+++ oclint-rules/rules/unused/UnusedLocalVariableRule.cpp
@@ -1,13 +1,58 @@
 #include <sstream>
+#include <set>
+#include <string>
 #include "oclint/AbstractASTVisitorRule.h"
 #include "oclint/RuleSet.h"
+#include "oclint/RuleConfiguration.h"
 
 using namespace std;
 using namespace clang;
 using namespace oclint;
 
+static set<string> skippedTypes;
+
+static void handleClassName(const string & className)
+{
+    //TODO verify the input
+    skippedTypes.insert(className);
+}
+
+static void splitClasses(const string & input) {
+    int startPos = 0;
+	// Take the string end into account, to avoid special string end handling
+    for (int i=0; i<input.length()+1; ++i)
+    {
+        for (auto const & curSep : { ',', ';', ' ', '\t', '\0' })
+        {
+            if (input[i] == curSep)
+            {
+                if (startPos != i)
+                {
+                    handleClassName(input.substr(startPos, i-startPos));
+                }
+                startPos = i+1;
+            }
+        }
+    }
+}
+
 class UnusedLocalVariableRule : public AbstractASTVisitorRule<UnusedLocalVariableRule>
 {
+public:
+    UnusedLocalVariableRule()
+    {
+        string defKeys = RuleConfiguration::stringForKey("RAII_DEFAULT_CLASSES",
+            "std::lock_guard, std::unique_lock");
+        string cusKeys = RuleConfiguration::stringForKey("RAII_CUSTOM_CLASSES", "");
+
+        for (auto const & curKeys : { defKeys, cusKeys })
+        {
+            if (!curKeys.empty())
+            {
+                splitClasses(curKeys);
+            }
+        }
+    }
 private:
     bool isInNonTemplateFunction(Decl *varDecl)
     {
@@ -70,7 +115,23 @@ public:
     {
         if (isUnusedLocalVariable(varDecl))
         {
-            addViolation(varDecl, this, description(varDecl->getNameAsString()));
+            // Getting ride of any template definition which might follow
+            auto varTypeName = varDecl->getType().getCanonicalType().getAsString();
+            auto const templPos = varTypeName.find('<');
+            if (templPos != string::npos) {
+                varTypeName = varTypeName.substr(0,templPos);
+            }
+            // Remove of the qualifiers, to get ride of class/struct/... definition parts
+            {
+                auto const lastSpacePos = varTypeName.rfind(' ');
+                if (lastSpacePos != string::npos) {
+                    varTypeName = varTypeName.substr(lastSpacePos + 1);
+                }
+            }
+            if (skippedTypes.find(varTypeName) == skippedTypes.end())
+            {
+                addViolation(varDecl, this, description(varDecl->getNameAsString()));
+            }
         }
         return true;
     }
