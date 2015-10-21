347ba7e56a2bdbccbcfd010fb0ff040a42d621c7
Martin Güthle <martin.guethle@bachmann-technology.com>
2015-10-22 01:30:51 +0200

    Appende a few test, to ensure that the RAII part is working with default and custom classes
    This parts cover:
    - Default usage with std::lock_guard and std::unique_ptr support
    - Defining additional own:
      * With full namespace given
      * Without full namespace
    - Remove the default definition

--- oclint-rules/test/unused/UnusedLocalVariableRuleTest.cpp
+++ oclint-rules/test/unused/UnusedLocalVariableRuleTest.cpp
@@ -73,8 +73,53 @@ public:\n\
     lock_guard(mutex_type& m, adopt_lock_t);\n\
     ~lock_guard();\n\
 };\n\
+template <class Mutex>\n\
+class unique_lock\n\
+{\n\
+public:\n\
+    typedef Mutex mutex_type;\n\
+    explicit unique_lock(mutex_type& m);\n\
+    unique_lock(mutex_type& m, adopt_lock_t);\n\
+    ~unique_lock();\n\
+};\n\
 adopt_lock_t adopt_lock;\n\
 } // namespace std\n";
+
+static const std::string stdMutexCodePre = stdMutexHeader + "\
+class TestClassA\n\
+{\n\
+};\n\
+namespace testNamespace\n\
+{\n\
+class TestClassB\n\
+{\n\
+};\n\
+}\n\
+int m() {\n\
+    std::mutex mtx;\n\
+";
+
+static const std::string stdMutexCodePost = "\n\
+    return 5;\n\
+}";
+
+
+/*
+ Define a test which is used to the UnusedLocalVariableRule class
+
+ This ensures, that it is possible, to set and modify the RAII classes taken into
+ account, while keep the possiblity to run the tests in parallel. At least the
+ GoogleTest manual advice to do it in this way.
+*/
+class UnusedLocalVariableRuleTest : public UnusedLocalVariableRule
+{
+public:
+    UnusedLocalVariableRuleTest(const std::list<std::string> & newSuppressClasses)
+    {
+        resetSkippedTypes(newSuppressClasses);
+    }
+};
+
 
 TEST(UnusedLocalVariableRuleTest, IgnoreRAIITechniqueInWhitelist)
 {
@@ -83,9 +128,104 @@ TEST(UnusedLocalVariableRuleTest, IgnoreRAIITechniqueInWhitelist)
 
 TEST(UnusedLocalVariableRuleTest, RAIITechniqueWhitelistDifferentNumOfParameters)
 {
-    testRuleOnCXXCode(new UnusedLocalVariableRule(), stdMutexHeader + "int m() { static std::mutex mutex; std::lock_guard<std::mutex> lock(mutex, std::adopt_lock); return 1; }",
-        0, 21, 36, 21, 91, "The local variable 'lock' is unused.");
+    testRuleOnCXXCode(new UnusedLocalVariableRule(), stdMutexHeader + "int m() { static std::mutex mutex; std::lock_guard<std::mutex> lock(mutex, std::adopt_lock); return 1; }");
 }
+
+TEST(UnusedLocalVariableRuleTest, RAIITechniqueDefaultParameters)
+{
+    testRuleOnCXXCode(new UnusedLocalVariableRule(),
+        stdMutexCodePre +
+            "{ std::lock_guard<std::mutex> lock (mtx);  }\n"
+            "{ std::unique_lock<std::mutex> lock (mtx); }\n"
+            VIOLATION_START "TestClassA " VIOLATION_END "testObjA;\n"
+            VIOLATION_START "testNamespace::TestClassB " VIOLATION_END "testObjB;\n"
+            VIOLATION_START "int iii = " VIOLATION_END "12;\n"
+        + stdMutexCodePost,
+        {
+            "The local variable 'testObjA' is unused.",
+            "The local variable 'testObjB' is unused.",
+            "The local variable 'iii' is unused.",
+        }
+    );
+}
+
+TEST(UnusedLocalVariableRuleTest, RAIITechniqueDefaultParametersExplicite)
+{
+    testRuleOnCXXCode(new UnusedLocalVariableRuleTest({"std::lock_guard std::unique_lock"}),
+        stdMutexCodePre +
+            "{ std::lock_guard<std::mutex> lock (mtx);  }\n"
+            "{ std::unique_lock<std::mutex> lock (mtx); }\n"
+            VIOLATION_START "TestClassA " VIOLATION_END "testObjA;\n"
+            VIOLATION_START "testNamespace::TestClassB " VIOLATION_END "testObjB;\n"
+            VIOLATION_START "int iii = " VIOLATION_END "12;\n"
+        + stdMutexCodePost,
+        {
+            "The local variable 'testObjA' is unused.",
+            "The local variable 'testObjB' is unused.",
+            "The local variable 'iii' is unused.",
+        }
+    );
+}
+
+
+TEST(UnusedLocalVariableRuleTest, RAIITechniqueCustomValuesWithoutFullNamespace)
+{
+    testRuleOnCXXCode(new UnusedLocalVariableRuleTest({"std::lock_guard std::unique_lock", "TestClassA TestClassB"}),
+        stdMutexCodePre +
+            "{ std::lock_guard<std::mutex> lock (mtx);  }\n"
+            "{ std::unique_lock<std::mutex> lock (mtx); }\n"
+            "TestClassA testObjA;\n"
+            VIOLATION_START "testNamespace::TestClassB " VIOLATION_END "testObjB;\n"
+            VIOLATION_START "int iii = " VIOLATION_END "12;\n"
+        + stdMutexCodePost,
+        {
+            "The local variable 'testObjB' is unused.",
+            "The local variable 'iii' is unused.",
+        }
+    );
+}
+
+
+TEST(UnusedLocalVariableRuleTest, RAIITechniqueCustomValuesWithFullNamespace)
+{
+    testRuleOnCXXCode(new UnusedLocalVariableRuleTest({
+        "std::lock_guard std::unique_lock",
+        "TestClassA testNamespace::TestClassB"
+    }),
+        stdMutexCodePre +
+            "{ std::lock_guard<std::mutex> lock (mtx);  }\n"
+            "{ std::unique_lock<std::mutex> lock (mtx); }\n"
+            "TestClassA testObjA;\n"
+            "testNamespace::TestClassB testObjB;\n"
+            VIOLATION_START "int iii = " VIOLATION_END "12;\n"
+        + stdMutexCodePost,
+        {
+            "The local variable 'iii' is unused.",
+        }
+    );
+}
+
+TEST(UnusedLocalVariableRuleTest, RAIITechniqueCustomValuesWithFullNamespaceWithoutDefault)
+{
+    testRuleOnCXXCode(new UnusedLocalVariableRuleTest({
+        "",
+        "TestClassA testNamespace::TestClassB"
+    }),
+        stdMutexCodePre +
+            "{" VIOLATION_START "std::lock_guard<std::mutex>  lock (mtx" VIOLATION_END "); }\n"
+            "{" VIOLATION_START "std::unique_lock<std::mutex> lock (mtx" VIOLATION_END "); }\n"
+            "TestClassA testObjA;\n"
+            "testNamespace::TestClassB testObjB;\n"
+            VIOLATION_START "int iii = " VIOLATION_END "12;\n"
+        + stdMutexCodePost,
+        {
+            "The local variable 'lock' is unused.",
+            "The local variable 'lock' is unused.",
+            "The local variable 'iii' is unused.",
+        }
+    );
+}
+
 
 TEST(UnusedLocalVariableRuleTest, SuppressUnusedLocalVariable)
 {
