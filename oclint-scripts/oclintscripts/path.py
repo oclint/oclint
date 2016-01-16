57729a670f67abbdc5b2c3332f18313d1569a5a3
Ryuichi Saito <ryuichi@ryuichisaito.com>
2016-01-16 10:14:57 -0800

    Be able to setup prebuilt llvm/clang binaries

--- oclint-scripts/oclintscripts/path.py
+++ oclint-scripts/oclintscripts/path.py
@@ -72,6 +72,9 @@ class url:
     json_compilation_database = 'https://github.com/oclint/oclint-json-compilation-database.git'
     xcodebuild = 'https://github.com/oclint/oclint-xcodebuild.git'
 
+    clang_prebuilt_binary_for_darwin = 'http://llvm.org/releases/3.7.0/clang+llvm-3.7.0-x86_64-apple-darwin.tar.xz'
+    clang_prebuilt_binary_for_ubuntu_lts = 'http://llvm.org/releases/3.7.0/clang+llvm-3.7.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz'
+
 def cd(dir_path):
     os.chdir(dir_path)
 
