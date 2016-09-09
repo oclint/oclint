be18244b20b44901c0285142a19a9a5792bae9f0
Ryuichi Saito <ryuichi@ryuichisaito.com>
2016-09-09 09:55:40 -0500

    Change the working folder back; explanation message

--- oclint-scripts/oclintscripts/version.py
+++ oclint-scripts/oclintscripts/version.py
@@ -7,13 +7,15 @@ from oclintscripts import path
 from oclintscripts import environment
 
 def dev_version():
+    current_working_folder = os.getcwd()
     try:
-        current_working_folder = os.getcwd()
         os.chdir(path.root_dir)
         git_hash = subprocess.check_output(['git', 'log', '-n', '1', '--pretty=%h']).split()[0]
         path.cd(current_working_folder)
         return git_hash
     except:
+        print("Ignore the git error above if you are building from a source code download instead of a git clone.")
+        path.cd(current_working_folder)
         return "src"
 
 def oclint_version():
